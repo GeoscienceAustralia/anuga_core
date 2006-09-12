08ab92543510adbd5f9e5ec384aeea9275e4e71c
Ole <Ole.Moller.Nielsen@gmail.com>
2006-09-12 16:49:52 +1100

    Moved more supporting files into shallow_water. Moved old least_squares to obsolete_code

---
+++ source/anuga/shallow_water/__init__.py
@@ -0,0 +1,15 @@
+"""Make directory available as a Python package
+"""
+
+# Add path of package to PYTHONPATH to allow C-extensions to be loaded
+import sys
+sys.path += __path__
+
+# Make selected classes available directly
+from shallow_water_domain import Domain,\
+     Transmissive_boundary, Reflective_boundary,\
+     Dirichlet_boundary, Time_boundary, File_boundary,\
+     Transmissive_Momentum_Set_Stage_boundary
+
+
+
