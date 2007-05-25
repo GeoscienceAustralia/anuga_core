88678db733e5cf267f85cfcb9fe9f03e4cf1f86a
nick <nick@ga.gov.au>
2007-05-25 14:03:03 +1100

    re-add lone_vert tests that i accidentally removed

--- source/anuga/abstract_2d_finite_volumes/test_util.py
+++ source/anuga/abstract_2d_finite_volumes/test_util.py
@@ -1243,6 +1243,53 @@ class Test_Util(unittest.TestCase):
     def test_check_list(self):
 
         check_list(['stage','xmomentum'])
+        
+    def test_remove_lone_verts_d(self):
+        verts = [[0,0],[1,0],[0,1]]
+        tris = [[0,1,2]]
+        new_verts, new_tris = remove_lone_verts(verts, tris)
+        assert new_verts == verts
+        assert new_tris == tris
+     
+
+    def test_remove_lone_verts_e(self):
+        verts = [[0,0],[1,0],[0,1],[99,99]]
+        tris = [[0,1,2]]
+        new_verts, new_tris = remove_lone_verts(verts, tris)
+        assert new_verts == verts[0:3]
+        assert new_tris == tris
+        
+    def test_remove_lone_verts_a(self):
+        verts = [[99,99],[0,0],[1,0],[99,99],[0,1],[99,99]]
+        tris = [[1,2,4]]
+        new_verts, new_tris = remove_lone_verts(verts, tris)
+        #print "new_verts", new_verts
+        assert new_verts == [[0,0],[1,0],[0,1]]
+        assert new_tris == [[0,1,2]]
+     
+    def test_remove_lone_verts_c(self):
+        verts = [[0,0],[1,0],[99,99],[0,1]]
+        tris = [[0,1,3]]
+        new_verts, new_tris = remove_lone_verts(verts, tris)
+        #print "new_verts", new_verts
+        assert new_verts == [[0,0],[1,0],[0,1]]
+        assert new_tris == [[0,1,2]]
+        
+    def test_remove_lone_verts_b(self):
+        verts = [[0,0],[1,0],[0,1],[99,99],[99,99],[99,99]]
+        tris = [[0,1,2]]
+        new_verts, new_tris = remove_lone_verts(verts, tris)
+        assert new_verts == verts[0:3]
+        assert new_tris == tris
+     
+
+    def test_remove_lone_verts_e(self):
+        verts = [[0,0],[1,0],[0,1],[99,99]]
+        tris = [[0,1,2]]
+        new_verts, new_tris = remove_lone_verts(verts, tris)
+        assert new_verts == verts[0:3]
+        assert new_tris == tris
+
     
         
 #-------------------------------------------------------------
