e1adf8ec6b9ba29f999f333c2097949537d58539
Ole <Ole.Moller.Nielsen@gmail.com>
2007-11-08 17:31:52 +1100

    Added auto validations to distribution (ticket:207) and updated installation guide as well as test_all
    	Changes to

--- test_all.py
+++ test_all.py
@@ -9,4 +9,15 @@ print 'Changing to', os.getcwd() #This is now different from buildroot
 
 execfile('test_all.py')
 
+print
+print '************************** NOTE *************************************'
+print 'If all unit tests passed you should run the suite of validation tests'
+print 'Go to the directory anuga_validation/automated_validation_tests'
+print 'and run'
+print '    python validate_all.py'
+print
+print 'These tests will take a few hours and will verify that ANUGA'
+print 'produces the physical results expected.'
+print '*********************************************************************'
+
     
