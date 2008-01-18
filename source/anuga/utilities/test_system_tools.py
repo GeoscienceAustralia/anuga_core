b26dad593a9c5157d1c614436d3c30c0c890b502
Ole <Ole.Moller.Nielsen@gmail.com>
2008-01-18 15:58:24 +1100

    Work on CRC32 for IP tracking

--- source/anuga/utilities/test_system_tools.py
+++ source/anuga/utilities/test_system_tools.py
@@ -2,7 +2,8 @@
 
 
 import unittest
-from Numeric import zeros, array, allclose
+from Numeric import zeros, array, allclose, Float
+from Scientific.IO.NetCDF import NetCDFFile
 
 from system_tools import *
 
@@ -24,20 +25,74 @@ class Test_system_tools(unittest.TestCase):
 
         # print host
         assert isinstance(host, basestring), 'User name should be a string'        
+
     def test_compute_checksum(self):
         """test_compute_checksum(self):
 
         Check that checksums on files are OK
         """
-        # FIXME: Not Done Yet
 
-        from tempfile import mkstemp
+        import zlib
+        from tempfile import NamedTemporaryFile, mkstemp, mktemp
 
         # Generate a text file
-        fd, pathname = mkstemp('.tmp', '', '.', '')
+        fid = open(mktemp(suffix='.tmp',
+                          dir='.'), 'w')
+        string = 'My temp file with textual content. AAAABBBBCCCC1234'
+        fid.write(string)
+        fid.close()
+
+
+        ref_crc = zlib.crc32(string)
+        checksum = compute_checksum(fid.name)
+
+        assert checksum == ref_crc
+
+        os.remove(fid.name)
+
+        # Binary file
+        fid = open(mktemp(suffix='.tmp',
+                          dir='.'), 'wb')
+        string = 'My temp file with textual content. AAAABBBBCCCC1234'
+        fid.write(string)
+        fid.close()
+
+        ref_crc = zlib.crc32(string)
+        checksum = compute_checksum(fid.name)
+
+        assert checksum == ref_crc
+        os.remove(fid.name)
+        
+        # Binary NetCDF File X 2
+
+        test_array = array([[7.0, 3.14], [-31.333, 0.0]])
+
+        # First file
+        filename1 = mktemp(suffix='.nc', dir='.')
+        fid = NetCDFFile(filename1, 'w')
+        fid.createDimension('two', 2)
+        fid.createVariable('test_array', Float,
+                           ('two', 'two'))
+        fid.variables['test_array'][:] = test_array
+        fid.close()
+
+        # Second file
+        filename2 = mktemp(suffix='.nc', dir='.')
+        fid = NetCDFFile(filename2, 'w')
+        fid.createDimension('two', 2)
+        fid.createVariable('test_array', Float,
+                           ('two', 'two'))
+        fid.variables['test_array'][:] = test_array
+        fid.close()
 
         
-        
+        checksum1 = compute_checksum(filename1)
+        checksum2 = compute_checksum(filename2)        
+        assert checksum1 == checksum2
+
+
+        os.remove(filename1)
+        os.remove(filename2)
 
         
 #-------------------------------------------------------------
