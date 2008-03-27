95e3c93822803574e01b87df1da853d2662f825b
sexton <jane.sexton@ga.gov.au>
2008-03-27 13:46:53 +1100

    test function for Okada earthquake source function

---
+++ source/anuga/shallow_water/test_eq.py
@@ -0,0 +1,66 @@
+import unittest
+from Numeric import allclose
+from eqf import earthquake_tsunami, Okada_func
+
+class Test_eq(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    def test_Okada_func(self):
+        x0 = 38000.0
+        y0 = 12500.0
+        length = 25.0
+        width = 5.0
+        strike = 0.0
+        depth = 3500.
+        slip = 10.0
+        dip = 15.0
+        rake = 90.0
+        test = -2205.904774487
+
+        print 'depth', depth
+        z = Okada_func(length=length, width=width, dip=dip, \
+                       x0=x0, y0=y0, strike=strike, depth=depth, \
+                       slip=slip, rake=rake, test=test)
+
+        assert allclose(z.length, length)
+        assert allclose(z.width, width)
+        assert allclose(z.x0, x0)
+        assert allclose(z.y0, y0)
+        assert allclose(z.strike, strike)
+        assert allclose(z.depth, depth)
+        assert allclose(z.slip, slip)
+        assert allclose(z.dip, dip)
+        assert allclose(z.rake, rake)
+
+        print 'in test', z.test
+        assert allclose(z.test, -2205.904774487)
+        
+        print 'hello finished okada'
+
+    def test_earthquake_tsunami(self):
+
+        x0 = 0.0
+        y0 = 0.0
+        length = 25.0
+        width = 5.0
+        strike = 0.0
+        depth = 3500.
+        slip = 10.0
+        dip = 15.0
+        
+        eq = earthquake_tsunami(length=length, width = width, depth=depth, \
+                                strike = strike, dip = dip, slip = slip, \
+                                x0 = x0, y0 = y0)
+
+#-------------------------------------------------------------
+if __name__ == "__main__":
+    suite = unittest.makeSuite(Test_eq,'test_Okada_func')
+    #suite = unittest.makeSuite(Test_eq,'test_earthquake_tsunami')
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
