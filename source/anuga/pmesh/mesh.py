358e33a37622947193b7b76d318a83b714b7db43
duncan <duncan.gray@ga.gov.au>
2008-04-11 16:18:26 +1100

    Adding regional tags when importing polygons

--- source/anuga/pmesh/mesh.py
+++ source/anuga/pmesh/mesh.py
@@ -582,7 +582,7 @@ class Mesh:
         self.regions.append(h)
         return h
    
-    def add_region(self, x,y, geo_reference=None):
+    def add_region(self, x,y, geo_reference=None, tag=None):
         """
         adds a point, which represents a region.
 
@@ -593,7 +593,10 @@ class Mesh:
         # but still return the instance, just in case.
         [[x,y]] = self.geo_reference.change_points_geo_ref([x,y],
                                                  points_geo_ref=geo_reference)
-        return self._addRegion(x, y)
+        region =  self._addRegion(x, y)
+        if tag is not None:
+            region.setTag(tag)
+        return region
 
     def build_grid(self,  vert_rows, vert_columns):
         """
@@ -679,7 +682,7 @@ class Mesh:
         This returns the region instance (if a max_triangle_area is given),
         so if the user whats to modify it they can.     
         """
-        if max_triangle_area is None:
+        if max_triangle_area is None and region_tag is None:
             create_region = False
         else:
             create_region = True
@@ -778,7 +781,6 @@ class Mesh:
         #Try None.
         # Due to this default this method is too connected to
         # _add_area_from_polygon
-        
         segment_tags = ['']*number_of_segs
         if tags is not None:
             for key in tags:
@@ -1880,12 +1882,17 @@ class Mesh:
         
 
 
-    def import_ungenerate_file(self,ofile, tag=None):
+    def import_ungenerate_file(self,ofile, tag=None, region_tag=None):
         """
         Imports an ungenerate file, from arcGIS into mesh.
 
         ofile is the name of the ungenerated file.
-        Tag is a string name to be taggged on each segment. 
+        Tag is a string name to be taggged on each segment.
+        
+        region_tag is the tag applied to the building regions.
+        if it is a string the one value will be assigned to all regions
+        if it is a list the first value in the list will be applied to the first polygon etc.
+        WARNING: size of list and number of polygons isn't checked 
 
         WARNING values are assumed to be absolute.
         geo-refs are not taken into account..
@@ -1895,10 +1902,21 @@ class Mesh:
         default_tag = Segment.get_default_tag()
         if tag is not None:
             Segment.set_default_tag(str(tag))
-        self.addVertsSegs(dict)
+        
+        if region_tag is None:
+            self.addVertsSegs(dict)
+        else:           
+            if not isinstance(region_tag, list):
+                region_tag = [region_tag]*len(dict['polygons'])
+            for a_tag,polygon in map(None, region_tag, dict['polygons']): 
+                segment_tags = {tag:range(len(polygon))}
+                self.add_region_from_polygon(polygon,segment_tags=segment_tags,
+                                region_tag=a_tag)
+            
+        
         Segment.set_default_tag(default_tag)
 
-        # change the tag back to  it's default
+        # change the tag back to it's default
     
         
 ########### IO CONVERTERS ##################
@@ -2163,18 +2181,23 @@ def readUngenerateFile(fd):
     Following lines: <x> <y> 
     last line:  "END"
     """
+    
     END_DELIMITER = 'END'
     
     points = []
     segments = []
+    polygons = []
     
     isEnd = False
     line = fd.readline() #not used <# of polynomial> <x> <y>
     while not isEnd:
+        poly = []
         line = fd.readline()
         fragments = line.split()
-        vert = [float(fragments.pop(0)),float(fragments.pop(0))]
-        points.append(vert)
+        x = float(fragments.pop(0))
+        y = float(fragments.pop(0))
+        points.append([x,y])
+        poly.append([x,y])
         PreviousVertIndex = len(points)-1
         firstVertIndex = PreviousVertIndex
         
@@ -2182,14 +2205,15 @@ def readUngenerateFile(fd):
         while not line.startswith(END_DELIMITER): 
             #print "line >" + line + "<"
             fragments = line.split()
-            vert = [float(fragments.pop(0)),float(fragments.pop(0))]
-            points.append(vert)
+            x = float(fragments.pop(0))
+            y = float(fragments.pop(0))
+            points.append([x,y])
+            poly.append([x,y])
             thisVertIndex = len(points)-1
             segment = [PreviousVertIndex,thisVertIndex]
             segments.append(segment)
             PreviousVertIndex = thisVertIndex
             line = fd.readline() #Read the next line
-            i =+ 1
         # If the last and first segments are the same,
         # Remove the last segment and the last vertex
         # then add a segment from the second last vert to the 1st vert
@@ -2201,11 +2225,14 @@ def readUngenerateFile(fd):
         if (firstVert[0] == thisVert[0] and firstVert[1] == thisVert[1]):
             points.pop()
             segments.pop()
+            poly.pop()
             thisVertIndex = len(points)-1
-            segments.append([thisVertIndex, firstVertIndex])
+        segments.append([thisVertIndex, firstVertIndex])
         
         line = fd.readline() # read <# of polynomial> <x> <y> OR END
         #print "line >>" + line + "<<"
+        # do poly stuff here
+        polygons.append(poly)
         if line.startswith(END_DELIMITER):
             isEnd = True
     
@@ -2214,6 +2241,7 @@ def readUngenerateFile(fd):
     ungenerated_dict = {}
     ungenerated_dict['points'] = points
     ungenerated_dict['segments'] = segments
+    ungenerated_dict['polygons'] = polygons
     return ungenerated_dict
 
 def importMeshFromFile(ofile):
