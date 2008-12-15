e5c2e1245d37201e9a4718662564978227c2c6d3
Ole <Ole.Moller.Nielsen@gmail.com>
2008-12-15 14:12:19 +1100

    Added two unit tests for culvert_class. One passes and one fails due to flows generated in a dry bed.

---
+++ source/anuga/culvert_flows/test_culvert_class.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+
+
+import unittest
+import os.path
+
+from anuga.utilities.system_tools import get_pathname_from_package        
+from anuga.abstract_2d_finite_volumes.mesh_factory import rectangular_cross
+
+from anuga.shallow_water import Domain, Reflective_boundary,\
+    Dirichlet_boundary,\
+    Transmissive_boundary, Time_boundary
+
+from anuga.culvert_flows.culvert_class import Culvert_flow_rating
+from anuga.culvert_flows.culvert_routines import boyd_generalised_culvert_model
+     
+from math import pi,pow,sqrt
+from Numeric import choose, greater, ones, sin, exp, cosh, allclose
+
+
+
+class Test_Culvert(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    def test_that_culvert_runs(self):
+        """test_that_culvert_runs
+        
+        This test only exercises the culvert - there is no quantitative 
+        diagnostics yet
+        """
+
+        path = get_pathname_from_package('anuga.culvert_flows')    
+        
+        length = 40.
+        width = 5.
+
+        dx = dy = 1           # Resolution: Length of subdivisions on both axes
+
+        points, vertices, boundary = rectangular_cross(int(length/dx),
+                                                       int(width/dy),
+                                                       len1=length, 
+                                                       len2=width)
+        domain = Domain(points, vertices, boundary)   
+        domain.set_name('Test_culvert')                 # Output name
+        domain.set_default_order(2)
+
+
+        #----------------------------------------------------------------------
+        # Setup initial conditions
+        #----------------------------------------------------------------------
+
+        def topography(x, y):
+            """Set up a weir
+            
+            A culvert will connect either side
+            """
+            # General Slope of Topography
+            z=-x/1000
+            
+            N = len(x)
+            for i in range(N):
+
+               # Sloping Embankment Across Channel
+                if 5.0 < x[i] < 10.1:
+                    # Cut Out Segment for Culvert FACE                
+                    if  1.0+(x[i]-5.0)/5.0 <  y[i]  < 4.0 - (x[i]-5.0)/5.0: 
+                       z[i]=z[i]
+                    else:
+                       z[i] +=  0.5*(x[i] -5.0)    # Sloping Segment  U/S Face
+                if 10.0 < x[i] < 12.1:
+                   z[i] +=  2.5                    # Flat Crest of Embankment
+                if 12.0 < x[i] < 14.5:
+                    # Cut Out Segment for Culvert FACE                
+                    if  2.0-(x[i]-12.0)/2.5 <  y[i]  < 3.0 + (x[i]-12.0)/2.5:
+                       z[i]=z[i]
+                    else:
+                       z[i] +=  2.5-1.0*(x[i] -12.0) # Sloping D/S Face
+                		   
+        		
+            return z
+
+
+        domain.set_quantity('elevation', topography) 
+        domain.set_quantity('friction', 0.01)         # Constant friction 
+        domain.set_quantity('stage',
+                            expression='elevation')   # Dry initial condition
+
+        filename=os.path.join(path, 'example_rating_curve.csv')
+        culvert = Culvert_flow_rating(domain,
+                               culvert_description_filename=filename,        
+                               end_point0=[9.0, 2.5], 
+                               end_point1=[13.0, 2.5],
+                               verbose=False)
+
+        domain.forcing_terms.append(culvert)
+        
+
+        #-----------------------------------------------------------------------
+        # Setup boundary conditions
+        #-----------------------------------------------------------------------
+
+        # Inflow based on Flow Depth and Approaching Momentum
+        Bi = Dirichlet_boundary([0.0, 0.0, 0.0])
+        Br = Reflective_boundary(domain)              # Solid reflective wall
+        Bo = Dirichlet_boundary([-5, 0, 0])           # Outflow
+        Btus = Time_boundary(domain, lambda t: [0.0+ 1.25*(1+sin(2*pi*(t-4)/10)), 0.0, 0.0])
+        Btds = Time_boundary(domain, lambda t: [0.0+ 0.75*(1+sin(2*pi*(t-4)/20)), 0.0, 0.0])
+        domain.set_boundary({'left': Btus, 'right': Btds, 'top': Br, 'bottom': Br})
+
+
+        #-----------------------------------------------------------------------
+        # Evolve system through time
+        #-----------------------------------------------------------------------
+
+        for t in domain.evolve(yieldstep = 1, finaltime = 25):
+            #print domain.timestepping_statistics()
+            pass
+    
+
+
+    def test_that_culvert_dry_bed(self):
+        """test_that_culvert_in_dry_bed_does_not_produce_flow(self):
+        
+        Test that culvert on a sloping dry bed doesn't produce flows
+        although there will be a 'pressure' head due to delta_w > 0
+        """
+
+        path = get_pathname_from_package('anuga.culvert_flows')    
+        
+        length = 40.
+        width = 5.
+
+        dx = dy = 1           # Resolution: Length of subdivisions on both axes
+
+        points, vertices, boundary = rectangular_cross(int(length/dx),
+                                                       int(width/dy),
+                                                       len1=length, 
+                                                       len2=width)
+        domain = Domain(points, vertices, boundary)   
+        domain.set_name('Test_culvert_dry') # Output name
+        domain.set_default_order(2)
+
+
+        #----------------------------------------------------------------------
+        # Setup initial conditions
+        #----------------------------------------------------------------------
+
+        def topography(x, y):
+            """Set up a weir
+            
+            A culvert will connect either side
+            """
+            # General Slope of Topography
+            z=-x/1000
+            
+            N = len(x)
+            for i in range(N):
+
+               # Sloping Embankment Across Channel
+                if 5.0 < x[i] < 10.1:
+                    # Cut Out Segment for Culvert FACE                
+                    if  1.0+(x[i]-5.0)/5.0 <  y[i]  < 4.0 - (x[i]-5.0)/5.0: 
+                       z[i]=z[i]
+                    else:
+                       z[i] +=  0.5*(x[i] -5.0)    # Sloping Segment  U/S Face
+                if 10.0 < x[i] < 12.1:
+                   z[i] +=  2.5                    # Flat Crest of Embankment
+                if 12.0 < x[i] < 14.5:
+                    # Cut Out Segment for Culvert FACE                
+                    if  2.0-(x[i]-12.0)/2.5 <  y[i]  < 3.0 + (x[i]-12.0)/2.5:
+                       z[i]=z[i]
+                    else:
+                       z[i] +=  2.5-1.0*(x[i] -12.0) # Sloping D/S Face
+                		   
+        		
+            return z
+
+
+        domain.set_quantity('elevation', topography) 
+        domain.set_quantity('friction', 0.01)         # Constant friction 
+        domain.set_quantity('stage',
+                            expression='elevation')   # Dry initial condition
+
+
+        filename = os.path.join(path, 'example_rating_curve.csv')
+        culvert = Culvert_flow_rating(domain,
+                               culvert_description_filename=filename,
+                               end_point0=[9.0, 2.5], 
+                               end_point1=[13.0, 2.5],
+                               verbose=False)
+
+        domain.forcing_terms.append(culvert)
+        
+
+        #-----------------------------------------------------------------------
+        # Setup boundary conditions
+        #-----------------------------------------------------------------------
+
+        # Inflow based on Flow Depth and Approaching Momentum
+
+        Br = Reflective_boundary(domain)              # Solid reflective wall
+        domain.set_boundary({'left': Br, 'right': Br, 'top': Br, 'bottom': Br})
+
+
+        #-----------------------------------------------------------------------
+        # Evolve system through time
+        #-----------------------------------------------------------------------
+
+        ref_volume = domain.get_quantity('stage').get_integral()
+        for t in domain.evolve(yieldstep = 1, finaltime = 25):
+            print domain.timestepping_statistics()
+            new_volume = domain.get_quantity('stage').get_integral()
+            
+            msg = 'Total volume has changed'
+            assert allclose(new_volume, ref_volume), msg
+            pass
+    
+
+    
+               
+#-------------------------------------------------------------
+if __name__ == "__main__":
+    suite = unittest.makeSuite(Test_Culvert, 'test')
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+        
