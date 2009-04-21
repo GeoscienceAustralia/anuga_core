0516d1b7245af1500508ca3957452d6e293df737
Ole <Ole.Moller.Nielsen@gmail.com>
2009-04-21 13:36:48 +1100

    Did propset on version.py

--- source/anuga/version.py
+++ source/anuga/version.py
@@ -1,12 +1,12 @@
 # Subversion keywords:
         
-author_string = '$Author: ole $'
-date_string = '$Date: 2009-03-28 18:54:15 +1100 (Sat, 28 Mar 2009) $'
-revision_string = '#$LastChangedRevision: 6654 $'
+author_string = '$Author$'
+date_string = '$Date$'
+revision_string = '#$LastChangedRevision$'
 
-#$LastChangedDate: 2009-03-28 18:54:15 +1100 (Sat, 28 Mar 2009) $
-#$LastChangedRevision: 6654 $
-#$LastChangedBy: ole $
+#$LastChangedDate$
+#$LastChangedRevision$
+#$LastChangedBy$
 
 def get_version():
     """Extract version info from keyword strings
