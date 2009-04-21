4b9305e0d83bd12b958321a0e89c978b1caacbd0
Ole <Ole.Moller.Nielsen@gmail.com>
2009-04-21 13:34:41 +1100

    Added stub for subversion keyword based method of getting revision numbers

---
+++ source/anuga/version.py
@@ -0,0 +1,15 @@
+# Subversion keywords:
+        
+author_string = '$Author: ole $'
+date_string = '$Date: 2009-03-28 18:54:15 +1100 (Sat, 28 Mar 2009) $'
+revision_string = '#$LastChangedRevision: 6654 $'
+
+#$LastChangedDate: 2009-03-28 18:54:15 +1100 (Sat, 28 Mar 2009) $
+#$LastChangedRevision: 6654 $
+#$LastChangedBy: ole $
+
+def get_version():
+    """Extract version info from keyword strings
+    """
+    
+    pass
