74e65127d2f1f5107cdd9a70b461f7d3f838711d
steve <stephen.roberts@anu.edu.au>
2009-09-01 10:37:04 +1100

    Forgot to add interface.py

---
+++ source/anuga_parallel/interface.py
@@ -0,0 +1,15 @@
+"""This is the public API to ANUGA_PARALLEL.
+
+Ideally, all tools needed to run paralllel simulations should be 
+imported from this module or anuga.interface
+"""
+
+from anuga_parallel.parallel_api import distribute
+from anuga_parallel.parallel_api import myid, numprocs, get_processor_name
+from anuga_parallel.parallel_api import send, receive
+from anuga_parallel.parallel_api import pypar_available, barrier
+
+from anuga_parallel.parallel_meshes import parallel_rectangle
+
+from parallel_shallow_water import Parallel_domain as Parallel_shallow_water_domain
+from parallel_advection     import Parallel_domain as Parallel_advection_domain
