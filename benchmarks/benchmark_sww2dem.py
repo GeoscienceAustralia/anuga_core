ea17aa7d47084641fbd4df545f53d81cabf22804
hudson <hudson@ga.gov.au>
2010-04-28 09:36:26 +1100

    Added profiling info to benchmark.

--- benchmarks/benchmark_sww2dem.py
+++ benchmarks/benchmark_sww2dem.py
@@ -6,24 +6,30 @@ Creates and exports a dem from an sww file.
 from anuga.shallow_water.data_manager import sww2dem
 from create_test_sww import create_test_sww
 import os.path
+import cProfile
 
-sww_name = 'test.sww'
 
-# use existing file
-if not os.path.isfile(sww_name):
-	create_test_sww(sww_name)
+def sww2dem_test():
+	sww_name = 'test.sww'
 
-# do export to DEM
+	# use existing file
+	if not os.path.isfile(sww_name):
+		create_test_sww(sww_name)
 
-sww2dem('test',
-        basename_out='sww2dem_out',
-        quantity='stage',
-        cellsize=1,      
-        easting_min=0,
-        easting_max=100,
-        northing_min=0,
-        northing_max=100,        
-        reduction=max, 
-        verbose=True,
-        format='asc')
+	# do export to DEM
 
+	sww2dem('test',
+			basename_out='sww2dem_out',
+			quantity='stage',
+			cellsize=1,      
+			easting_min=0,
+			easting_max=100,
+			northing_min=0,
+			northing_max=100,        
+			reduction=max, 
+			verbose=True,
+			format='asc')
+
+			
+			
+cProfile.run('sww2dem_test()')
