d1bf68ca14b68ffa7a785dffa861cf7268fd57ea
hudson <hudson@ga.gov.au>
2010-05-05 16:13:54 +1100

    Modified benchmark to give profiling info.

--- benchmarks/benchmark_sww2dem.py
+++ benchmarks/benchmark_sww2dem.py
@@ -7,21 +7,18 @@ from anuga.shallow_water.data_manager import sww2dem
 from create_test_sww import create_test_sww
 import os.path
 import cProfile
+import time
 
+
+sww_name = 'test.sww'
 
 def sww2dem_test():
-	sww_name = 'test.sww'
-
-	# use existing file
-	if not os.path.isfile(sww_name):
-		create_test_sww(sww_name)
-
 	# do export to DEM
 
 	sww2dem('test',
 			basename_out='sww2dem_out',
 			quantity='stage',
-			cellsize=1,      
+			cellsize=0.25,      
 			easting_min=0,
 			easting_max=100,
 			northing_min=0,
@@ -30,6 +27,13 @@ def sww2dem_test():
 			verbose=True,
 			format='asc')
 
-			
-			
+		
+# use existing file
+if not os.path.isfile(sww_name):
+	create_test_sww(sww_name)
+
+start_time = time.time()	
 cProfile.run('sww2dem_test()')
+#sww2dem_test()
+stop_time = time.time()
+print ('sww2dem took %.1fs\n\n\n' % (stop_time - start_time))
