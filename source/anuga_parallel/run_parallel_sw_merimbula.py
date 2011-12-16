8ba852cb9d09203bc0e3cf100e84c928b5404ae4
steve <stephen.roberts@anu.edu.au>
2011-12-16 20:42:04 +1100

    Changed final time in simulation

--- source/anuga_parallel/run_parallel_sw_merimbula.py
+++ source/anuga_parallel/run_parallel_sw_merimbula.py
@@ -46,7 +46,7 @@ mesh_filename = "merimbula_10785_1.tsh" ; x0 = 756000.0 ; x1 = 756500.0
 #mesh_filename = "test-100.tsh" ; x0 = 0.25 ; x1 = 0.5
 #mesh_filename = "test-20.tsh" ; x0 = 250.0 ; x1 = 350.0
 yieldstep = 20
-finaltime = 1000
+finaltime = 200
 verbose = True
 
 #--------------------------------------------------------------------------
