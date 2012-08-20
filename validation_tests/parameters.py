74c47a80247ccbd49ba8cb6d3494b5c8a1546ef4
steve <stephen.roberts@anu.edu.au>
2012-08-21 07:45:08 +1100

    Let's try passing parameters via the parmeters file.

---
+++ validation_tests/parameters.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="stephen"
+__date__ ="$20/08/2012 11:20:00 PM$"
+
+
+alg = '2_0'
+cfl = 1.0
+
+
