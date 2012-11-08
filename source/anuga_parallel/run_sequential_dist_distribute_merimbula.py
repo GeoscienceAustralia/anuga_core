1a5b48ec42b908a537fb598f51d42723ae155345
steve <stephen.roberts@anu.edu.au>
2012-11-08 14:17:25 +1100

    Some code to run sequential distribute

---
+++ source/anuga_parallel/run_sequential_dist_distribute_merimbula.py
@@ -0,0 +1,101 @@
+
+"""Run parallel shallow water domain.
+
+   run using command like:
+
+   mpirun -np m python run_parallel_sw_merimbula.py
+
+   where m is the number of processors to be used.
+   
+   Will produce sww files with names domain_Pn_m.sww where m is number of processors and
+   n in [0, m-1] refers to specific processor that owned this part of the partitioned mesh.
+"""
+
+#------------------------------------------------------------------------------
+# Import necessary modules
+#------------------------------------------------------------------------------
+
+import os
+import sys
+import time
+import numpy as num
+
+#------------------------
+# ANUGA Modules
+#------------------------
+	
+from anuga import Domain
+from anuga import Reflective_boundary
+from anuga import Dirichlet_boundary
+from anuga import Time_boundary
+from anuga import Transmissive_boundary
+
+from anuga import rectangular_cross
+from anuga import create_domain_from_file
+
+
+from anuga_parallel.sequential_distribute import sequential_distribute
+
+
+#--------------------------------------------------------------------------
+# Setup parameters
+#--------------------------------------------------------------------------
+
+#mesh_filename = "merimbula_10785_1.tsh" ; x0 = 756000.0 ; x1 = 756500.0
+mesh_filename = "merimbula_17156.tsh"   ; x0 = 756000.0 ; x1 = 756500.0
+#mesh_filename = "merimbula_43200_1.tsh"   ; x0 = 756000.0 ; x1 = 756500.0
+#mesh_filename = "test-100.tsh" ; x0 = 0.25 ; x1 = 0.5
+#mesh_filename = "test-20.tsh" ; x0 = 250.0 ; x1 = 350.0
+yieldstep = 50
+finaltime = 1500
+verbose = True
+
+#--------------------------------------------------------------------------
+# Setup procedures
+#--------------------------------------------------------------------------
+class Set_Stage:
+    """Set an initial condition with constant water height, for x0<x<x1
+    """
+
+    def __init__(self, x0=0.25, x1=0.5, h=1.0):
+        self.x0 = x0
+        self.x1 = x1
+        self.h  = h
+
+    def __call__(self, x, y):
+        return self.h*((x>self.x0)&(x<self.x1))+1.0
+
+
+class Set_Elevation:
+    """Set an elevation
+    """
+
+    def __init__(self, h=1.0):
+        self.x0 = x0
+        self.x1 = x1
+        self.h  = h
+
+    def __call__(self, x, y):
+        return x/self.h
+    
+
+#--------------------------------------------------------------------------
+# Setup Sequential Domain
+#--------------------------------------------------------------------------
+domain = create_domain_from_file(mesh_filename)
+domain.set_quantity('stage', Set_Stage(x0, x1, 2.0))
+#domain.set_datadir('.')
+domain.set_name('merimbula_new')
+domain.set_store(True)
+
+
+#--------------------------------------------------------------------------
+# Distribute sequential domain on processor 0 to other processors
+#--------------------------------------------------------------------------
+
+if verbose: print 'DISTRIBUTING DOMAIN'
+sequential_distribute(domain, 4, verbose=True)
+
+
+
+
