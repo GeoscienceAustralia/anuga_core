f4449da974839aa834945c0481c977acb06cd149
mungkasi <sudi.mungkasi@anu.edu.au>
2012-11-22 23:41:32 +1100

    Adding an avalanche test involving a shock wave.

---
+++ validation_tests/Tests/Simple/avalanche_wet/analytical_avalanche_wet.py
@@ -0,0 +1,115 @@
+"""
+Analytical solution to debris avalanche involving a shock wave
+Ref: Mungkasi and Roberts, Pure and Applied Geophysics 2012
+"""
+
+from numpy import array, zeros, linspace
+from numpy import arctan, sqrt, sin, cos, tan
+
+#Parameters
+g = 9.81           # gravity
+h_0 = 20.0         # depth upstream
+h_1 = 10.0         # depth downstream
+L = 200.0          # length of stream/domain
+
+
+#delt = 0.0
+friction_slope = 0.05             #tan(delta) # NOTE THAT friction_slope must less than bed_slope
+#thet = pi/36.0
+bed_slope = 0.1                   #tan(theta) #0.1      # bottom slope, positive if it is increasing bottom.
+thet = arctan(bed_slope)
+F2 = g*cos(thet)*cos(thet)*friction_slope
+c0 = sqrt(g*h_0)                  # sound speed
+m = -1.0*g*bed_slope + F2         # auxiliary variable
+
+
+# Function for analytical solution
+def analytical_sol(X,t):
+    #The following is for computing the shock velocity
+    c0 = sqrt(g*h_0) #right celerity
+    c1 = sqrt(g*h_1) #left celerity
+    Smin=-21.0
+    Smax=20.0
+    for i in range(100):
+        S=(Smin+Smax)/2.0       
+        u2=S-c1*c1/4.0/S*(1.0+sqrt(1.0+8.0*S*S/c1/c1))
+        c2=c1*sqrt(0.5*(sqrt(1.0+8.0*S*S/c1/c1)-1.0))
+        func = -2.0*c0 -u2 +2.0*c2
+        if (func > 0.0):
+            Smin=S
+        else:
+            Smax=S
+    if( abs(S) > 99.0):
+        print 'no convergence'
+    #h2= h_1/(1.0-u2/S)
+    h2 = c2**2/9.81
+    #print "S=",S
+    #print "c2=",c2
+    #print "u2=",u2
+    
+    N = len(X)
+    u = zeros(N)
+    h = zeros(N)
+    w = zeros(N)
+    z = zeros(N)
+    mom = zeros(N)
+    if abs(t)>1e-9:
+        for i in range(N):
+            # Calculate Analytical Solution at time t > 0
+            if X[i]-0.5*m*t**2 <= S*t :
+                u[i] = m*t 
+                h[i] = h_1
+            elif X[i]-0.5*m*t**2 <= (u2+c2)*t :
+                u[i] = u2+m*t
+                h[i] = h2
+            elif X[i]-0.5*m*t**2 < c0*t :
+                u[i] = 2.0/3.0*((X[i]-0.5*m*t**2)/t - sqrt(g*h_0))  +m*t
+                h[i] = 4.0/(9.0*g)*((X[i]-0.5*m*t**2)/(2.0*t) + sqrt(g*h_0))**2
+            else:
+                u[i] = m*t
+                h[i] = h_0 
+            z[i] = bed_slope*X[i]
+            w[i] = h[i] + z[i]
+            mom[i] = u[i]*h[i]
+    else:
+        for i in range(N):
+            # Calculate Analytical Solution at time t > 0
+            if X[i] <= 0.0:
+                u[i] = 0.0
+                h[i] = h_1
+            else:
+                u[i] = 0.0
+                h[i] = h_0
+            z[i] = bed_slope*X[i]
+            w[i] = h[i] + z[i]
+            mom[i] = u[i]*h[i]    
+    return u,h,w,z,mom
+
+"""
+# Run a simulation
+n = 800            # number of cells
+cell_len = L/n     # length of each cell
+points = zeros(n+1)
+for i in range (n+1):
+    points[i] = i*cell_len - 0.5*L
+X = points
+t = 4.0
+#The following is for plotting the result.
+Uv,Hv,Wv,Zv,Mv = analytical_sol(X,t)
+from pylab import clf,plot,title,xlabel,ylabel,legend,savefig,show,hold,subplot
+hold(False)
+clf()
+
+plot1 = subplot(211)
+plot(X,Wv,'b-', X,Zv,'k-')
+ylabel('Stage')
+#plot1.set_ylim([-50.0,75.0])#([-1.0,21.0])
+legend(('analytical solution', 'bed elevation'), 'upper left', shadow=False)
+
+plot2 = subplot(212)
+plot(X,Mv,'b-')
+xlabel('Position')
+ylabel('Momentum')
+#plot2.set_ylim([-200.0,10.0])#([-90.0,10.0])   
+show()
+"""
---
+++ validation_tests/Tests/Simple/avalanche_wet/numerical_avalanche_wet.py
@@ -0,0 +1,171 @@
+"""
+Numerical solution to debris avalanche involving a shock wave
+Ref: Mungkasi and Roberts, Pure and Applied Geophysics 2012
+"""
+
+#------------------------------------------------------------------------------
+# Import necessary modules
+#------------------------------------------------------------------------------
+import sys
+import anuga
+from anuga import Domain as Domain
+from math import cos
+from numpy import zeros
+from time import localtime, strftime, gmtime
+from numpy import sin, cos, tan, arctan
+
+
+#-------------------------------------------------------------------------------
+# Copy scripts to time stamped output directory and capture screen
+# output to file
+#-------------------------------------------------------------------------------
+time = strftime('%Y%m%d_%H%M%S',localtime())
+
+#output_dir = 'avalanche_'+time
+output_dir = '.'
+output_file = 'avalanche'
+
+#anuga.copy_code_files(output_dir,__file__)
+#start_screen_catcher(output_dir+'_')
+
+
+#------------------------------------------------------------------------------
+# Setup domain
+#------------------------------------------------------------------------------
+g = 9.81           # gravity
+h_0 = 20.0         # depth upstream
+h_1 = 10.0         # depth downstream
+L = 200.0          # length of stream/domain
+dx = 0.5
+dy = dx
+W = 3*dx
+
+
+# structured mesh
+points, vertices, boundary = anuga.rectangular_cross(int(L/dx), int(W/dy), L, W, (-L/2.0, -W/2.0))
+
+#domain = anuga.Domain(points, vertices, boundary) 
+domain = Domain(points, vertices, boundary) 
+
+domain.set_name(output_file)                
+domain.set_datadir(output_dir) 
+
+#------------------------------------------------------------------------------
+# Setup Algorithm, either using command line arguments
+# or override manually yourself
+#------------------------------------------------------------------------------
+from anuga.utilities.argparsing import parse_standard_args
+alg, cfl = parse_standard_args()
+domain.set_flow_algorithm(alg)
+domain.set_CFL(cfl)
+
+#------------------------------------------------------------------------------
+# Setup initial conditions
+#------------------------------------------------------------------------------
+
+# No Mannings friction, but we introduce Coulomb friction below.
+domain.set_quantity('friction', 0.0)
+class Coulomb_friction:
+    
+    def __init__(self,
+                 friction_slope=0.05,
+                 bed_slope=0.1):
+        
+        self.friction_slope = friction_slope   #tan(delta) # NOTE THAT friction_slope must less than bed_slope
+        self.bed_slope = bed_slope             #tan(theta) # bottom slope, positive if it is increasing bottom.
+
+        thet = arctan(bed_slope)
+        self.F = g*cos(thet)*cos(thet)*friction_slope
+        self.m = -1.0*g*bed_slope + self.F
+        
+    def __call__(self, domain):        
+
+        w = domain.quantities['stage'].centroid_values
+        z = domain.quantities['elevation'].centroid_values
+        h = w-z
+
+        xmom_update = domain.quantities['xmomentum'].explicit_update
+        ymom_update = domain.quantities['ymomentum'].explicit_update
+
+        xmom_update[:] = xmom_update + self.F*h
+
+
+bed_slope = 0.1
+friction_slope = 0.05
+Coulomb_forcing_term = Coulomb_friction(friction_slope, bed_slope)
+domain.forcing_terms.append(Coulomb_forcing_term)
+
+def stage(X,Y):
+    N = len(X)
+    w = zeros(N)
+    for i in range(N):
+        if X[i]<=0.0:
+            w[i] = bed_slope*X[i] + h_1
+        else:
+            w[i] = bed_slope*X[i] + h_0
+    return w
+domain.set_quantity('stage', stage)
+
+def elevation(X,Y):
+    N = len(X)
+    y=zeros(N)
+    for i in range(N):
+        y[i] = bed_slope*X[i]
+    return y
+domain.set_quantity('elevation',elevation)
+
+def f_right(t):
+    z_r = bed_slope*(0.5*L)
+    h_r = h_0 #+ bed_slope*cell_len
+    w_r = z_r + h_r
+    u_r = Coulomb_forcing_term.m*t
+    #['stage', 'xmomentum', 'ymomentum']
+    return [w_r,  u_r*h_r,  0.0]
+
+def f_left(t):
+    z_l = bed_slope*(-0.5*L)
+    h_l = h_1 #+ bed_slope*cell_len
+    w_l = z_l + h_l
+    u_l = Coulomb_forcing_term.m*t
+    #['stage', 'xmomentum', 'ymomentum']
+    return [w_l,  u_l*h_l,  0.0]
+
+
+
+
+#-----------------------------------------------------------------------------
+# Setup boundary conditions
+#------------------------------------------------------------------------------
+from math import sin, pi, exp
+Br = anuga.Reflective_boundary(domain)      # Solid reflective wall
+#Bt = anuga.Transmissive_boundary(domain)    # Continue all values on boundary 
+#Bd = anuga.Dirichlet_boundary([1,0.,0.]) # Constant boundary values
+BTimeR = anuga.Time_boundary(domain,f_right)
+BTimeL = anuga.Time_boundary(domain,f_left)
+
+# Associate boundary tags with boundary objects
+domain.set_boundary({'left': BTimeL, 'right': BTimeR, 'top': Br, 'bottom': Br})
+
+
+#===============================================================================
+##from anuga.visualiser import RealtimeVisualiser
+##vis = RealtimeVisualiser(domain)
+##vis.render_quantity_height("stage", zScale =h0*500, dynamic=True)
+##vis.colour_height_quantity('stage', (0.0, 0.5, 1.0))
+##vis.start()
+#===============================================================================
+
+
+#------------------------------------------------------------------------------
+# Evolve system through time
+#------------------------------------------------------------------------------
+for t in domain.evolve(yieldstep = 0.1, finaltime = 4.):
+    #print domain.timestepping_statistics(track_speeds=True)
+    print domain.timestepping_statistics()
+    #vis.update()
+
+
+#test against know data
+    
+#vis.evolveFinished()
+
