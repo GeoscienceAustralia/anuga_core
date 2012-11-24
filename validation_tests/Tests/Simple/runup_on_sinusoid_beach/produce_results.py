bfe771c1a0692cb56090a4a7db795a3fa183bf59
mungkasi <sudi.mungkasi@anu.edu.au>
2012-11-25 01:40:39 +1100

    Uploading the updated version of Gareth's sinusoid beach runup.

---
+++ validation_tests/Tests/Simple/runup_on_sinusoid_beach/produce_results.py
@@ -0,0 +1,18 @@
+#--------------------------------
+# import modules
+#--------------------------------
+from fabricate import *
+from validation_tests.utilities import run_validation_script
+
+# Setup the python scripts which produce the output for this
+# validation test
+def build():
+    run_validation_script('runup_sinusoid.py')
+    run_validation_script('plot_results.py')
+
+def clean():
+    autoclean()
+
+main()
+
+
