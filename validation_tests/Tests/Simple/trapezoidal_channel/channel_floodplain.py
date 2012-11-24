aa074650cd5c02485063854b5b37d6bf2ac3c4c1
mungkasi <sudi.mungkasi@anu.edu.au>
2012-11-24 17:04:24 +1100

    Deleting the old trapezoidal_channel and uploading the new one.

--- validation_tests/Tests/Simple/trapezoidal_channel/channel_floodplain.py
+++ validation_tests/Tests/Simple/trapezoidal_channel/channel_floodplain.py
@@ -1,4 +1,5 @@
-"""Simple water flow example using ANUGA
+"""
+Simple water flow example using ANUGA
 Water flowing down a channel with a floodplain
 """
 
@@ -14,19 +15,18 @@ from anuga import *
 #------------------------------------------------------------------------------
 # Useful parameters for controlling this case
 #------------------------------------------------------------------------------
-
 floodplain_length = 1000.0 # Model domain length
-floodplain_width = 14.0 # Model domain width
+floodplain_width = 14.0    # Model domain width
 floodplain_slope = 1./300.
-chan_initial_depth = 0.65 # Initial depth of water in the channel
-chan_bankfull_depth = 1.0 # Bankfull depth of the channel
-chan_width = 10.0 # Bankfull width of the channel
-bankwidth = 2. # Width of the bank regions -- note that these protrude into the channel
-man_n=0.03 # Manning's n
-l0 = 2.000 # Length scale associated with triangle side length in channel (min_triangle area = 0.5*l0^2)
+chan_initial_depth = 0.65  # Initial depth of water in the channel
+chan_bankfull_depth = 1.0  # Bankfull depth of the channel
+chan_width = 10.0          # Bankfull width of the channel
+bankwidth = 2.             # Width of the bank regions -- note that these protrude into the channel
+man_n=0.03                 # Manning's n
+l0 = 1.000 # Length scale associated with triangle side length in channel (min_triangle area = 0.5*l0^2)
 
 assert chan_width < floodplain_width, \
-        ' ERROR: Channel width is greater than floodplain width'
+        'ERROR: Channel width is greater than floodplain width'
 
 assert bankwidth < chan_width/2., \
         'ERROR: The bank width must be less than half the channel width'
@@ -34,7 +34,6 @@ assert bankwidth < chan_width/2., \
 #------------------------------------------------------------------------------
 # Setup computational domain
 #------------------------------------------------------------------------------
-
 # Define boundary polygon -- in this case clockwise around the proposed boundary
 boundary_polygon = [ [0.,0.], 
                      [0., floodplain_length], 
@@ -55,7 +54,6 @@ channel_polygon = [ [floodplain_width/2. - chan_width/2., +l0],
                     [floodplain_width/2. + chan_width/2., +l0]
                     ]
 
-
 # Define domain with appropriate boundary conditions
 domain = create_domain_from_regions( boundary_polygon, 
                                    boundary_tags={'left': [0],
@@ -75,7 +73,6 @@ domain = create_domain_from_regions( boundary_polygon,
                                    use_cache=False,
                                    verbose=True)
 
-
 domain.set_name('channel_floodplain') # Output name
 
 #------------------------------------------------------------------------------
@@ -94,12 +91,7 @@ domain.set_CFL(cfl)
 #------------------------------------------------------------------------------
 
 # Function for topography
-def topography(x, y):
-    # Longitudinally sloping floodplain with channel in centre
-	#return -y*floodplain_slope -chan_bankfull_depth*\
-    #        (x>(floodplain_width/2. - chan_width/2.) )*\
-    #        (x<(floodplain_width/2. + chan_width/2.) ) 
-
+def elevation(x, y):
     elev1= -y*floodplain_slope - chan_bankfull_depth*\
             (x>(floodplain_width/2. - chan_width/2.))*\
             (x<(floodplain_width/2. + chan_width/2.)) 
@@ -115,34 +107,27 @@ def topography(x, y):
         elev2 = elev2 + (chan_bankfull_depth \
                 + chan_bankfull_depth/bankwidth*(x - rightbnk))*\
                 (x>rightbnk-bankwidth)*(x < rightbnk)
-    
     if(bankwidth==0.0):
         elev2 = elev1
-    #
     return elev2
 
-#
 
 #Function for stage
-def stagetopo(x,y):
+def stage(x,y):
     return -y*floodplain_slope -chan_bankfull_depth + chan_initial_depth 
 
-domain.set_quantity('elevation', topography) # Use function for elevation
-domain.set_quantity('friction', man_n) # Constant friction
-domain.set_quantity('stage', stagetopo) # Use function for stage
-#domain.set_minimum_allowed_height(0.01) # 
+domain.set_quantity('elevation', elevation) # Use function for elevation
+domain.set_quantity('friction', man_n)      # Constant friction
+domain.set_quantity('stage', stage)         # Use function for stage
 
 # Define inlet operator 
 flow_in_yval=0.0
 if True:
     line1 = [ [floodplain_width/2. - chan_width/2., flow_in_yval],\
-              [floodplain_width/2. + chan_width/2., flow_in_yval] \
-              ]
+              [floodplain_width/2. + chan_width/2., flow_in_yval] ]
     Qin = 0.5*(floodplain_slope*(chan_width*chan_initial_depth)**2.*man_n**(-2.)\
             *chan_initial_depth**(4./3.) )**0.5
-    
     Inlet_operator(domain, line1, Qin)
-    
     print 'Discharge in = ', Qin 
 
 #------------------------------------------------------------------------------
@@ -152,7 +137,6 @@ if True:
 #------------------------------------------------------------------------------
 
 Br = anuga.Reflective_boundary(domain) # Solid reflective wall
-#Bt = anuga.Transmissive_boundary(domain) # Transmissive boundary
 
 def outflow_stage_boundary(t):
     return -floodplain_length*floodplain_slope \
@@ -200,7 +184,5 @@ for t in domain.evolve(yieldstep=2.0, finaltime=1200.0):
         
         print 'Cross sectional flows: ',s0, s1, s2 
         print '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
-        discharge_outfile.write(str(t) + "," +str(s0) + ","+ str(s1) +"," + str(s2) + "\n")        
-
-
+        discharge_outfile.write(str(t) + "," +str(s0) + ","+ str(s1) +"," + str(s2) + "\n")
 discharge_outfile.close()
