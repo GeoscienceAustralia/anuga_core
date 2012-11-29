044ce6bf539ec669df8ec26526da6fd4b4dea0a3
mungkasi <sudi.mungkasi@anu.edu.au>
2012-11-29 11:44:28 +1100

    Uploading a new version of water running down hill.

---
+++ validation_tests/Tests/Simple/rundown_mild_slope/plot_results.py
@@ -0,0 +1,95 @@
+"""
+View results of run_channel.py
+
+Simple water flow example using ANUGA: Water flowing down a channel.
+It was called "steep_slope" in an old validation test.
+"""
+#---------------
+# Import Modules
+#---------------
+import time
+import anuga
+import numpy
+import scipy
+from matplotlib import pyplot as pyplot
+from anuga.utilities import plot_utils as util
+#--------------
+# Get variables
+#--------------
+p=util.get_output('channel.sww', 0.001)
+p2=util.get_centroids(p,velocity_extrapolation=True)
+
+#------------------
+# Select line
+#------------------
+#v=(y==2.5)
+v=(p2.y==p2.y[3])
+
+#-------------------------------
+# Define variables of case study
+#-------------------------------
+mann=0.03 # Manning's coef
+bedslope=-0.1
+fluxin=20./100. #The momentum flux at the upstream boundary ( = discharge / width)
+
+#---------------------------------------------
+# Analytical solution for steady, uniform flow
+#---------------------------------------------
+# This comes from solving 
+# 1) Friction_slope=bedslope , and
+# 2) flux = depth*velocity = flux_in
+uana= ( mann**(-2.)*abs(bedslope)*fluxin**(4./3.) )**(3./10.) # Velocity
+dana= fluxin/uana # Depth
+
+#--------------------
+# Make plot animation
+#--------------------
+pyplot.clf()
+pyplot.ion()
+
+line, = pyplot.plot( (p2.x[v].min(),p2.x[v].max()) ,( (p2.stage[:,v]-p2.elev[:,v]).max(),(p2.stage[:,v]-p2.elev[v]).min() ) )
+line.set_label('numerical')
+pyplot.plot( (0,100),(dana,dana), 'r',label='analytical' )
+pyplot.legend()
+#pyplot.plot(x[v],elev[v],'green')
+for i in range(p2.xmom.shape[0]):
+    line.set_xdata(p2.x[v])
+    line.set_ydata(p2.stage[i,v]-p2.elev[v])
+    pyplot.draw()
+    pyplot.title('Flow depth: should be converging to steady uniform flow ' )
+    pyplot.xlabel('Xposition')
+    pyplot.ylabel('Depth')
+pyplot.ylim([0.085,0.095])
+pyplot.legend(loc='best')
+pyplot.title('Flow depth at 400s')# -- there should be a central region with steady uniform flow ' )
+pyplot.savefig('final_depth.png')
+
+#--------------------------------------------
+# Compare velocity with analytical solution
+#--------------------------------------------
+# Find an x value close to x==50
+tmp=(abs(p2.x-50.)).argmin()
+v=(p2.x==p2.x[tmp])
+
+pyplot.clf()
+pyplot.plot(p2.y[v],p2.xvel[100,v], 'o', label='numerical')
+pyplot.plot((0,100),(uana,uana),label='analytical')
+pyplot.ylim([1.0,3.0])
+pyplot.xlabel('Yposition along the line x=50')
+pyplot.ylabel('Xvelocity m/s')
+pyplot.title('Final Xvelocity around the line x=50.')
+pyplot.legend(loc='best')
+pyplot.savefig('x_velocity.png')
+
+pyplot.clf()
+pyplot.plot(p2.y[v],p2.yvel[100,v],'o', label='numerical')
+pyplot.plot((0,100),(0.0, 0.0),label='analytical')
+pyplot.xlabel('Yposition along the line x=50')
+pyplot.ylabel('Yvelocity')
+pyplot.title('Final Yvelocity around the line x=50.')
+pyplot.legend(loc='best')
+pyplot.savefig('y_velocity.png')
+
+
+
+
