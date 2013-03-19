17e2c0956c2efddc909b83e85d541f05022b2592
steve <stephen.roberts@anu.edu.au>
2013-03-19 13:17:49 +1100

    Just added another test to pymetis

--- source/anuga_parallel/pymetis/test_metis.py
+++ source/anuga_parallel/pymetis/test_metis.py
@@ -74,6 +74,42 @@ class TestMetis(unittest.TestCase):
         self.assert_(edgecut == 5)
         assert allclose(epart, epart_expected)
         assert allclose(npart, npart_expected)
+		
+		
+		
+    def test_Hexmesh3(self):
+        # Hexagonal mesh
+        #
+        #   1---2---7 
+        #  /5\0/1\6/7\
+        # 6---0---3---8
+        #  \4/3\2/9\8/
+        #   5---4---9
+        #
+        # Divided 2 ways
+        # Calling order is: elements, verticies, edge list
+        # element type, number parts
+        edgecut, epart, npart = metis.partMeshNodal(10, 10,\
+                                                    [0, 2, 1,\
+                                                     0, 3, 2,\
+                                                     0, 4, 3,\
+                                                     0, 5, 4,\
+                                                     0, 6, 5,\
+                                                     0, 1, 6,\
+													 3, 2, 7,\
+													 3, 7, 8,\
+													 3, 8, 9,\
+													 3, 9, 4],\
+                                                    1,\
+                                                    3,)
+        #print edgecut
+        #print epart
+        #print npart
+        epart_expected = array([0, 0, 0, 1, 2, 2, 2, 2, 1, 1], 'i')
+        npart_expected = array([0, 2, 0, 2, 1, 1, 2, 2, 0, 1], 'i')
+        self.assert_(edgecut == 14)
+        assert allclose(epart, epart_expected)
+        assert allclose(npart, npart_expected)
 
 
 if __name__ == "__main__":
