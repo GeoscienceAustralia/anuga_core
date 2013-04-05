6569825f9d31def7033955904bb54407fe20e91e
steve <stephen.roberts@anu.edu.au>
2013-04-05 17:41:16 +1100

    Turn scientific python netcdf back on

--- source/anuga/file/netcdf.py
+++ source/anuga/file/netcdf.py
@@ -41,22 +41,17 @@ def NetCDFFile(file_name, netcdf_mode=netcdf_mode_r):
     
     """
 
-    from netCDF4 import Dataset
-    if netcdf_mode == 'wl' :
-        return Dataset(file_name, 'w', format='NETCDF3_64BIT')
-    else:
-        return Dataset(file_name, netcdf_mode, format='NETCDF3_64BIT')
 
 
-#    try:
-#        from Scientific.IO.NetCDF import NetCDFFile
-#        return NetCDFFile(file_name, netcdf_mode)
-#    except:
-#        from netCDF4 import Dataset
-#        if netcdf_mode == 'wl' :
-#            return Dataset(file_name, 'w', format='NETCDF3_64BIT')
-#        else:
-#            return Dataset(file_name, netcdf_mode, format='NETCDF3_64BIT')
+    try:
+        from Scientific.IO.NetCDF import NetCDFFile
+        return NetCDFFile(file_name, netcdf_mode)
+    except:
+        from netCDF4 import Dataset
+        if netcdf_mode == 'wl' :
+            return Dataset(file_name, 'w', format='NETCDF3_64BIT')
+        else:
+            return Dataset(file_name, netcdf_mode, format='NETCDF3_64BIT')
 
 #    from netCDF4 import Dataset
 #    return Dataset(file_name, netcdf_mode, format='NETCDF3_64BIT')
