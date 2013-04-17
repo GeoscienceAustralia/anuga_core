450e1435ddc6b479f956088fe8153ea34a89c9e2
steve <stephen.roberts@anu.edu.au>
2013-04-17 14:55:43 +1100

    Changed compile_all.py in anuga_core to use compile_all.py in anuga directory

--- compile_all.py
+++ compile_all.py
@@ -17,54 +17,56 @@ t0 = time.time()
 
 # Attempt to compile all ANUGA extensions
 
-os.chdir('utilities')
-subprocess.call([sys.executable, 'compile.py', 'quad_tree.c'])
-subprocess.call([sys.executable, 'compile.py', 'sparse_dok.c'])
-subprocess.call([sys.executable, 'compile.py', 'sparse_csr.c'])
-execfile('compile.py')
+execfile('compile_all.py')
 
-os.chdir('..')
-os.chdir('advection')
-execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
-
-os.chdir('..')
-os.chdir('operators')
-execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
-
-os.chdir('..')
-os.chdir('file_conversion')
-execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
-
-os.chdir('..')
-os.chdir('geometry')
-execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
-
-os.chdir('..')
-os.chdir('structures')
-execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
-
-os.chdir('..')
-os.chdir('abstract_2d_finite_volumes')
-execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
-
-os.chdir('..')
-os.chdir('file')
-execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
-
-os.chdir('..')
-os.chdir('shallow_water')
-execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
-
-
-os.chdir('..')
-os.chdir('mesh_engine')
-execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
-
-os.chdir('..')
-os.chdir('fit_interpolate')
-subprocess.call([sys.executable, '..' + os.sep + 'utilities' + os.sep + 'compile.py', 'rand48.c'])
-subprocess.call([sys.executable, '..' + os.sep + 'utilities' + os.sep + 'compile.py', 'ptinpoly.c'])
-execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
+#os.chdir('utilities')
+#subprocess.call([sys.executable, 'compile.py', 'quad_tree.c'])
+#subprocess.call([sys.executable, 'compile.py', 'sparse_dok.c'])
+#subprocess.call([sys.executable, 'compile.py', 'sparse_csr.c'])
+#execfile('compile_all.py')
+#
+#os.chdir('..')
+#os.chdir('advection')
+#execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
+#
+#os.chdir('..')
+#os.chdir('operators')
+#execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
+#
+#os.chdir('..')
+#os.chdir('file_conversion')
+#execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
+#
+#os.chdir('..')
+#os.chdir('geometry')
+#execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
+#
+#os.chdir('..')
+#os.chdir('structures')
+#execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
+#
+#os.chdir('..')
+#os.chdir('abstract_2d_finite_volumes')
+#execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
+#
+#os.chdir('..')
+#os.chdir('file')
+#execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
+#
+#os.chdir('..')
+#os.chdir('shallow_water')
+#execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
+#
+#
+#os.chdir('..')
+#os.chdir('mesh_engine')
+#execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
+#
+#os.chdir('..')
+#os.chdir('fit_interpolate')
+#subprocess.call([sys.executable, '..' + os.sep + 'utilities' + os.sep + 'compile.py', 'rand48.c'])
+#subprocess.call([sys.executable, '..' + os.sep + 'utilities' + os.sep + 'compile.py', 'ptinpoly.c'])
+#execfile('..' + os.sep + 'utilities' + os.sep + 'compile.py')
 
 
 os.chdir(buildroot)    
