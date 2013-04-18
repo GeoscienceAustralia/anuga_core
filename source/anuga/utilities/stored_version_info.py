94cafc84e60377465dd95c40e5d51f67a3d31ab2
steve <stephen.roberts@anu.edu.au>
2013-04-18 23:04:28 +1100

    Backup stored value of version info

---
+++ source/anuga/utilities/stored_version_info.py
@@ -0,0 +1,19 @@
+"""Stored version info.
+
+This file provides the version for distributions that are not accessing Subversion directly.
+The file is automatically generated and should not be modified manually.
+"""
+
+version_info = """
+Path: .
+URL: https://anuga.anu.edu.au/svn/anuga/trunk/anuga_core/source/anuga/utilities
+Repository Root: https://anuga.anu.edu.au/svn/anuga
+Repository UUID: e22b01d3-8fe4-0310-a5c0-ca58b0ebee9c
+Revision: 8834
+Node Kind: directory
+Schedule: normal
+Last Changed Author: steve
+Last Changed Rev: 8820
+Last Changed Date: 2013-04-08 21:28:42 +1000 (Mon, 08 Apr 2013)
+
+"""
