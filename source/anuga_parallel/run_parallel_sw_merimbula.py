affb60a1accd3908a42bc34c0fafcedb8cd9fc49
steve <stephen.roberts@anu.edu.au>
2013-05-18 22:42:06 +1100

    Small changes to run_parallel_sw_merimbula

--- source/anuga_parallel/run_parallel_sw_merimbula.py
+++ source/anuga_parallel/run_parallel_sw_merimbula.py
@@ -42,10 +42,10 @@ from anuga_parallel import distribute, myid, numprocs, finalize, barrier
 # Setup parameters
 #--------------------------------------------------------------------------
 
-mesh_filename = "merimbula_10785_1.tsh" ; x0 = 756000.0 ; x1 = 756500.0; yieldstep = 50; finaltime = 1000
+mesh_filename = "merimbula_10785_1.tsh" ; x0 = 756000.0 ; x1 = 756500.0; yieldstep = 10; finaltime = 1000
 #mesh_filename = "merimbula_17156.tsh"   ; x0 = 756000.0 ; x1 = 756500.0; yieldstep = 50; finaltime = 500
 #mesh_filename = "merimbula_43200_1.tsh"   ; x0 = 756000.0 ; x1 = 756500.0; yieldstep = 50; finaltime = 500
-mesh_filename = "test-100.tsh" ; x0 = 200.0 ; x1 = 300.0; yieldstep = 1; finaltime = 50
+#mesh_filename = "test-100.tsh" ; x0 = 200.0 ; x1 = 300.0; yieldstep = 1; finaltime = 50
 #mesh_filename = "test-20.tsh" ; x0 = 250.0 ; x1 = 350.0; yieldstep = 1; finaltime = 50
 
 verbose = True
@@ -171,9 +171,9 @@ if myid == 0 and verbose: print 'EVOLVE'
 barrier()
 t0 = time.time()
 for t in domain.evolve(yieldstep = yieldstep, finaltime = finaltime):
-    print 'P:'+ str(myid) + ' '+ domain.timestepping_statistics()
-    #if myid == 0:
-    #    domain.write_time()
+    #print 'P:'+ str(myid) + ' '+ domain.timestepping_statistics()
+    if myid == 0:
+        domain.write_time()
 
 
 barrier()
