5012a72fe422711549fcb15edf6e4ab69f39dee6
steve <stephen.roberts@anu.edu.au>
2014-06-12 08:17:54 +1100

    Changed name of validation script

--- run_auto_validation_tests.py
+++ run_auto_validation_tests.py
@@ -1,11 +1,11 @@
 import os
 
-os.chdir('source')
-os.chdir('anuga_validation_tests')
+
+os.chdir('validation_tests')
 print
 print 20*'=' + ' anuga automated validation tests ' + 20*'='
 print 'Changing to', os.getcwd() # This is now different from buildroot   
-execfile('validate_all.py')
+execfile('run_auto_validation_tests.py')
 
 
 
