e8e7bb4d6cc5611af9d5e95fbb4e946807f0c3cc
steve <stephen.roberts@anu.edu.au>
2014-06-15 17:22:06 +1100

    Combined plot_result scripts

--- validation_tests/analytical_exact/paraboloid_basin/numerical_paraboloid_basin.py
+++ validation_tests/analytical_exact/paraboloid_basin/numerical_paraboloid_basin.py
@@ -18,7 +18,7 @@ from math import sqrt, cos, sin, pi
 from numpy import asarray
 from time import localtime, strftime, gmtime
 from anuga.geometry.polygon import inside_polygon, is_inside_triangle
-
+from anuga import myid, finalize, distribute
 
 #-------------------------------------------------------------------------------
 # Copy scripts to time stamped output directory and capture screen
@@ -31,6 +31,10 @@ output_file = 'paraboloid'
 #start_screen_catcher(output_dir+'_')
 
 
+args = anuga.get_args()
+alg = args.alg
+verbose = args.v
+
 #-------------------------------------------------------------------------------
 # Domain
 #-------------------------------------------------------------------------------
@@ -40,81 +44,69 @@ lenx = 8000.0
 leny = 8000.0
 origin = (-4000.0, -4000.0)
 
-points, elements, boundary = anuga.rectangular_cross(m, n, lenx, leny, origin)
-domain = anuga.Domain(points, elements, boundary)
 
 
-#-------------------------------------------------------------------------------
-# Provide file name for storing output
-#-------------------------------------------------------------------------------
-domain.store = True #False
-domain.format = 'sww'
-domain.set_name(output_file)                
-domain.set_datadir(output_dir)
+#===============================================================================
+# Create Sequential Domain
+#===============================================================================
+if myid == 0:
+    points, elements, boundary = anuga.rectangular_cross(m, n, lenx, leny, origin)
+    domain = anuga.Domain(points, elements, boundary)
 
 
-#-------------------------------------------------------------------------------
-# Order of scheme
-# Good compromise between
-# limiting and CFL
-# CUT THIS FOR AUTOMATED TESTS
-#-------------------------------------------------------------------------------
-#domain.set_default_order(2)
-#domain.set_timestepping_method(2)
-#domain.set_beta(1.5)
-##domain.set_CFL(0.6)
-#domain.smooth = True
+    domain.set_name(output_file)                
+    domain.set_datadir(output_dir)
+    
+    domain.set_flow_algorithm(alg)
 
+    
+    #-------------------------------------------------------------------------------
+    # Initial conditions
+    #-------------------------------------------------------------------------------
+    t = 0.0
+    D0 = 1000.
+    L = 2500.
+    R0 = 2000.
+    
+    A = (L**4 - R0**4)/(L**4 + R0**4)
+    omega = 2./L*sqrt(2.*g*D0)
+    T = pi/omega
+    
+    #------------------
+    # Set bed elevation
+    def bed_elevation(x,y):
+        n = x.shape[0]
+        z = 0*x
+        for i in range(n):
+            r = sqrt(x[i]*x[i] + y[i]*y[i])
+            z[i] = -D0*(1.-r*r/L/L)
+        return z
+    domain.set_quantity('elevation', bed_elevation)
+    
+    #----------------------------
+    # Set the initial water level
+    def stage_init(x,y):
+        z = bed_elevation(x,y)
+        n = x.shape[0]
+        w = 0*x
+        for i in range(n):
+            r = sqrt(x[i]*x[i] + y[i]*y[i])
+            w[i] = D0*((sqrt(1-A*A))/(1.-A*cos(omega*t))
+                    -1.-r*r/L/L*((1.-A*A)/((1.-A*cos(omega*t))**2)-1.))
+        if w[i] < z[i]:
+            w[i] = z[i]
+        return w
+    domain.set_quantity('stage', stage_init)
 
-#-------------------------------------------------------------------------------
-# Decide which quantities are to be stored at each timestep
-#-------------------------------------------------------------------------------
-#domain.quantities_to_be_stored = ['stage', 'xmomentum', 'ymomentum']
+else:
+    
+    domain = None
+ 
+#============================================================================
+# Create Parallel Domain
+#============================================================================
+domain = distribute(domain)
 
-
-#-------------------------------------------------------------------------------
-# Reduction operation for get_vertex_values
-from anuga.utilities.numerical_tools import mean
-domain.reduction = mean #domain.reduction = min  #Looks better near steep slopes
-
-
-#-------------------------------------------------------------------------------
-# Initial condition
-#-------------------------------------------------------------------------------
-t = 0.0
-D0 = 1000.
-L = 2500.
-R0 = 2000.
-
-A = (L**4 - R0**4)/(L**4 + R0**4)
-omega = 2./L*sqrt(2.*g*D0)
-T = pi/omega
-
-#------------------
-# Set bed elevation
-def bed_elevation(x,y):
-    n = x.shape[0]
-    z = 0*x
-    for i in range(n):
-        r = sqrt(x[i]*x[i] + y[i]*y[i])
-        z[i] = -D0*(1.-r*r/L/L)
-    return z
-domain.set_quantity('elevation', bed_elevation)
-
-#----------------------------
-# Set the initial water level
-def stage_init(x,y):
-    z = bed_elevation(x,y)
-    n = x.shape[0]
-    w = 0*x
-    for i in range(n):
-        r = sqrt(x[i]*x[i] + y[i]*y[i])
-        w[i] = D0*((sqrt(1-A*A))/(1.-A*cos(omega*t))
-                -1.-r*r/L/L*((1.-A*A)/((1.-A*cos(omega*t))**2)-1.))
-    if w[i] < z[i]:
-        w[i] = z[i]
-    return w
-domain.set_quantity('stage', stage_init)
 
 #---------
 # Boundary
@@ -123,93 +115,25 @@ domain.set_quantity('stage', stage_init)
 D = anuga.Dirichlet_boundary([0.0, 0.0, 0.0])
 domain.set_boundary({'left': D, 'right': D, 'top': D, 'bottom': D})
 
-#---------------------------------------------
-# Find triangle that contains the point Point
-# and print to file
-#---------------------------------------------
-Point = (0.0, 0.0)
-for n in range(len(domain.triangles)):
-    tri = domain.get_vertex_coordinates(n)
-    if is_inside_triangle(Point,tri):
-        #print 'Point is within triangle with vertices '+'%s'%tri
-        n_point = n
-        break
-print 'n_point = ',n_point
-#bla
-    
-#t = domain.triangles[n_point]
-#print 't = ', t
-#tri = domain.get_vertex_coordinates(n)
-
-filename=domain.get_name()
-file = open(filename,'w')
-
-#----------
-# Evolution
-import time
-t0 = time.time()
-
-time_array = []
-stage_array = []
-Stage     = domain.quantities['stage']
-Xmomentum = domain.quantities['xmomentum']
-Ymomentum = domain.quantities['ymomentum']
-
-#interactive_visualisation = True
-#===============================================================================
-##if interactive_visualisation:
-##    from anuga.visualiser import RealtimeVisualiser
-##    vis = RealtimeVisualiser(domain)
-##    vis.render_quantity_height("stage", zScale =1000, dynamic=True)
-##    vis.colour_height_quantity('stage', (1.0, 0.5, 0.5))
-##    vis.start()
-#===============================================================================
-
 
 #------------------------------------------------------------------------------
 # Produce a documentation of parameters
 #------------------------------------------------------------------------------
-parameter_file=open('parameters.tex', 'w')
-parameter_file.write('\\begin{verbatim}\n')
-from pprint import pprint
-pprint(domain.get_algorithm_parameters(),parameter_file,indent=4)
-parameter_file.write('\\end{verbatim}\n')
-parameter_file.close()
+if myid == 0:
+    parameter_file=open('parameters.tex', 'w')
+    parameter_file.write('\\begin{verbatim}\n')
+    from pprint import pprint
+    pprint(domain.get_algorithm_parameters(),parameter_file,indent=4)
+    parameter_file.write('\\end{verbatim}\n')
+    parameter_file.close()
 
+import time
+t0 = time.time()
 for t in domain.evolve(yieldstep = 1.0, finaltime = 200.0):
-    domain.write_time()
+    if myid == 0 and verbose : domain.write_time()
 
-    #tri_array = asarray(tri)
-    #t_array = asarray([[0,1,2]])
-    #interp = Interpolation(tri_array,t_array,[points])
+if myid==0 and verbose: print 'That took %s secs' % str(time.time()- t0)
 
-    stage     = Stage.get_values(location='centroids',indices=[n_point])
-    xmomentum = Xmomentum.get_values(location='centroids',indices=[n_point])
-    ymomentum = Ymomentum.get_values(location='centroids',indices=[n_point])
-    #print '%10.6f   %10.6f  %10.6f   %10.6f\n'%(t,stage,xmomentum,ymomentum)
-    file.write( '%10.6f   %10.6f  %10.6f   %10.6f\n'%(t,stage,xmomentum,ymomentum) )
+domain.sww_merge(delete_old=True)
 
-    #time_array.append(t)
-    #stage_array.append(stage)
-
-    #if interactive_visualisation:
-    #    vis.update()
-"""
-file.close()
-print 'That took %.2f seconds' %(time.time()-t0)
-from pylab import *
-#ion()
-hold(False)
-plot(time_array, stage_array, 'r.-')
-#title('Gauge %s' %name)
-xlabel('time (s)')
-ylabel('stage (m)')
-#legend(('Observed', 'Modelled'), shadow=True, loc='upper left')
-#savefig(name, dpi = 300)
-
-#raw_input('Next')
-show()
-"""
-
-#if interactive_visualisation:
-#    vis.evolveFinished()
+finalize()
---
+++ validation_tests/analytical_exact/paraboloid_basin/plot_results.py
@@ -0,0 +1,106 @@
+"""
+    Quick plot of the dam break outputs
+    Note: v = p2.y[?], where ? is the cell position of the origin.
+
+"""
+import anuga.utilities.plot_utils as util
+from matplotlib import pyplot as pyplot
+#from math import sqrt, pi,sin,cos
+from numpy import sqrt, sin, cos, pi
+from numpy import ones
+from analytical_paraboloid_basin import analytic_sol
+
+# Get the sww file
+p = util.get_output('paraboloid.sww')
+p2=util.get_centroids(p)
+
+v = p2.y[11248]#[4898]
+v2=(p2.y==v)
+time_level = 50
+w2,u2,h2 = analytic_sol(p2.x[v2], v2, p2.time[time_level])
+
+
+#Plot stages
+pyplot.clf()
+pyplot.plot(p2.x[v2], p2.stage[time_level,v2],'b.', label='numerical stage')
+pyplot.plot(p2.x[v2], p2.elev[v2],'k-', label='bed elevation')
+pyplot.plot(p2.x[v2], w2,'r-', label='analytical stage')
+pyplot.title('Stage at an instant in time')
+pyplot.legend(loc='best')
+pyplot.xlabel('Xposition')
+pyplot.ylabel('Stage')
+pyplot.savefig('cross_section_stage.png')
+
+
+#Plot xmomentum
+pyplot.clf()
+pyplot.plot(p2.x[v2], p2.xmom[time_level,v2], 'b.', label='numerical')
+pyplot.plot(p2.x[v2], u2*h2,'r-', label='analytical')
+pyplot.title('Xmomentum at an instant in time')
+pyplot.legend(loc='best')
+pyplot.xlabel('Xposition')
+pyplot.ylabel('Xmomentum')
+pyplot.savefig('cross_section_xmom.png')
+
+
+#Plot velocities
+pyplot.clf()
+pyplot.plot(p2.x[v2], p2.xvel[time_level,v2], 'b.', label='numerical')
+pyplot.plot(p2.x[v2], u2,'r-', label='analytical')
+pyplot.title('Xvelocity at an instant in time')
+pyplot.legend(loc='best')
+pyplot.xlabel('Xposition')
+pyplot.ylabel('Xvelocity')
+pyplot.savefig('cross_section_xvel.png')
+
+
+# Parameters
+D0 = 1000.
+L = 2500.
+R0 = 2000.
+g = 9.81
+
+A = (L**4 - R0**4)/(L**4 + R0**4)
+omega = 2./L*sqrt(2.*g*D0)
+T = pi/omega
+index_origin = 11248#4898
+
+
+
+
+
+# Free surface over time at the origin of spatial domain.
+w = D0*((sqrt(1.-A*A))/(1.-A*cos(omega*p2.time)) -1.)
+
+#Plot stages
+pyplot.clf()
+pyplot.plot(p2.time,w, color='blue', label='analytical')
+pyplot.plot(p2.time,p2.stage[:,index_origin], color='green', label='numerical')
+pyplot.legend(loc='best')
+pyplot.title('Stage at the centre of the paraboloid basin over time')
+pyplot.xlabel('Time')
+pyplot.ylabel('Stage')
+pyplot.savefig('Stage_origin.png')
+
+#Plot xmomentums
+pyplot.clf()
+pyplot.plot(p2.time,0.0*p2.time, color='blue', label='analytical') # At the origin of space, w=h as always wet.
+pyplot.plot(p2.time,p2.xmom[:,index_origin], color='green', label='numerical')
+pyplot.legend(loc='best')
+pyplot.title('Xmomentum at the centre of the paraboloid basin over time')
+pyplot.xlabel('Time')
+pyplot.ylabel('Xmomentum')
+pyplot.savefig('Xmom_origin.png')
+
+#Plot velocities
+pyplot.clf()
+pyplot.plot(p2.time,0.0*p2.time, color='blue', label='analytical')
+pyplot.plot(p2.time,p2.xvel[:,index_origin], color='green', label='numerical')
+pyplot.legend(loc='best')
+pyplot.title('Xvelocity at the centre of the paraboloid basin over time')
+pyplot.xlabel('Time')
+pyplot.ylabel('Xvelocity')
+pyplot.savefig('Xvel_origin.png')
+
+
+
