c6227fc31a24f8dde54c23ec9c71c431089b7bd6
steve <stephen.roberts@anu.edu.au>
2014-06-17 22:12:51 +1100

    Adding in new module

---
+++ source/anuga/validation_utilities/produce_report.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python
+
+
+__author__="stephen"
+__date__ ="$20/08/2012 11:20:00 PM$"
+
+    
+from anuga import run_anuga_script
+from anuga.validation_utilities import typeset_report
+
+def produce_report(script, args=None):
+
+    import anuga
+
+    if args is None:
+        args  = anuga.get_args()
+        
+
+    verbose = args.verbose
+    
+    
+    
+    #print args
+    
+    # Get the arguments from the calling script
+
+    run_anuga_script(script, args=args)
+    
+    # We don't want to run plot_results in parallel
+    args.np = 1
+
+    run_anuga_script('plot_results.py', args=args)
+    typeset_report(verbose=verbose)
+
+    
+
+
