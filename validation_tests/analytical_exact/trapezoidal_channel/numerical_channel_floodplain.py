16ebc598b5b617fc7277d02746f587decc690587
steve <stephen.roberts@anu.edu.au>
2014-06-24 11:51:04 +1100

    Clean up of numerical script

--- validation_tests/analytical_exact/trapezoidal_channel/numerical_channel_floodplain.py
+++ validation_tests/analytical_exact/trapezoidal_channel/numerical_channel_floodplain.py
@@ -9,7 +9,6 @@ Water flowing down a channel with a floodplain
 # Import standard shallow water domain and standard boundaries.
 import anuga
 import numpy
-from anuga import Inlet_operator
 from anuga import myid, finalize, distribute
 
 #------------------------------------------------------------------------------
@@ -19,17 +18,16 @@ args = anuga.get_args()
 verbose = args.verbose
 alg = args.alg
 
-from project import *
+#--------------------------------
+# See project file for definitions of these
+# variables
+#--------------------------------
+from project import \
+          floodplain_length, floodplain_width, floodplain_slope,\
+          chan_initial_depth, chan_bankfull_depth, chan_width, bankwidth, \
+          man_n, l0
 
-# floodplain_length = 800.0 # Model domain length
-# floodplain_width = 14.0    # Model domain width
-# floodplain_slope = 1./300.
-# chan_initial_depth = 0.65  # Initial depth of water in the channel
-# chan_bankfull_depth = 1.0  # Bankfull depth of the channel
-# chan_width = 10.0          # Bankfull width of the channel
-# bankwidth = 2.             # Width of the bank regions -- note that these protrude into the channel
-# man_n=0.03                 # Manning's n
-# l0 = 1.00 # Length scale associated with triangle side length in channel (min_triangle area = 0.5*l0^2)
+#l0 = 2.0
 
 assert chan_width < floodplain_width, \
         'ERROR: Channel width is greater than floodplain width'
@@ -134,7 +132,7 @@ line1 = [ [floodplain_width/2. - chan_width/2., flow_in_yval],\
 
 Qin = 0.5*(floodplain_slope*(chan_width*chan_initial_depth)**2.*man_n**(-2.)\
             *chan_initial_depth**(4./3.) )**0.5
-Inlet_operator(domain, line1, Qin)
+anuga.Inlet_operator(domain, line1, Qin)
 
 if myid == 0 and verbose : print 'Discharge in = ', Qin 
 
@@ -151,7 +149,7 @@ def outflow_stage_boundary(t):
 # Note that the outflow boundary may be slightly incorrect for the trapezoidal channel case, 
 # or incorrect more generally if there are numerical problems. But, in the central regions of
 # the channel, this shouldn't prevent us reaching steady, uniform flow.
-Bout_tmss = anuga.shallow_water.boundaries.Transmissive_momentum_set_stage_boundary(domain, function = outflow_stage_boundary) 
+Bout_tmss = anuga.Transmissive_momentum_set_stage_boundary(domain, function = outflow_stage_boundary) 
 
 domain.set_boundary({'left': Br, 
                      'right': Br, 
@@ -161,10 +159,6 @@ domain.set_boundary({'left': Br,
                      'bottom2': Br, 
                      'chan_out': Bout_tmss, 
                      'chan_in': Br})
-
-# Set up file to record computations of discharge at several points.
-#discharge_outfile=open('discharge_outputs.txt', 'w')
-#discharge_outfile.write('Time (s)'+","+ 'Discharge@10' + ","+ 'Discharge@700'+","+ 'Discharge@1000' + "\n")
 
 
 #------------------------------------------------------------------------------
@@ -183,27 +177,6 @@ if myid == 0:
 #------------------------------------------------------------------------------
 for t in domain.evolve(yieldstep=10.0, finaltime=1000.0):
     if myid == 0 and verbose: print domain.timestepping_statistics()
-
-##     xx=domain.quantities['ymomentum'].centroid_values
-##     dd=(domain.quantities['stage'].centroid_values - domain.quantities['elevation'].centroid_values)
-##     dd=dd*(dd>0.)
-
-##     tmp = xx/(dd+1.0e-06)*(dd>0.0)
-##     print tmp.max(), tmp.argmax(), tmp.min(),  tmp.argmin()
-
-##     # Compute flow through cross-section -- check that the inflow boundary condition is doing its job
-##     # This also provides another useful steady-state check
-##     if( numpy.floor(t/100.) == t/100. ):
-##         print '#### COMPUTING FLOW THROUGH CROSS-SECTIONS########'
-##         s0 = domain.get_flow_through_cross_section([[0., 10.0], [floodplain_width, 10.0]])
-##         s1 = domain.get_flow_through_cross_section([[0., floodplain_length-300.0], [floodplain_width, floodplain_length-300.0]])
-##         s2 = domain.get_flow_through_cross_section([[0., floodplain_length-1.0], [floodplain_width, floodplain_length-1.0]])
-        
-##         print 'Cross sectional flows: ',s0, s1, s2 
-##         print '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
-##         discharge_outfile.write(str(t) + "," +str(s0) + ","+ str(s1) +"," + str(s2) + "\n")
-
-## discharge_outfile.close()
 
 domain.sww_merge(delete_old=True)
 
