39def49d439c5e6a72f7256a1deca54c3b56dbf8
steve <stephen.roberts@anu.edu.au>
2015-01-13 16:44:53 +1100

    Reducing size of merewether validation test

--- validation_tests/case_studies/merewether/produce_results.py
+++ validation_tests/case_studies/merewether/produce_results.py
@@ -1,5 +1,5 @@
 import os
 
-os.system('mpirun -np 6 python merewether.py -alg DE0')
+os.system('python runMerewether.py -alg DE0')
 os.system('python plot_results.py')
 os.system('pdflatex results.tex')
