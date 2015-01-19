6fdca4aef41ae287af58ba5e6e3e59d93e89458b
steve <stephen.roberts@anu.edu.au>
2015-01-19 14:39:59 +1100

    Moving report to documentation folder

--- source/anuga_parallel/hotshot-view.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-import hotshot.stats
-
-if len(sys.argv) == 2:
-    stats = hotshot.stats.load(sys.argv[1])
-    stats.strip_dirs().sort_stats('cumulative').print_stats(20)
-else:
-    print "Usage: %s profile_filename" % sys.argv[0]
