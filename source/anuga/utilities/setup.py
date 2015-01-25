90d79050e4da685ef3c9d712646fa814889b63b4
steve <stephen.roberts@anu.edu.au>
2015-01-25 20:31:24 +1100

    Adding in a setup.py script for utilities folder

---
+++ source/anuga/utilities/setup.py
@@ -0,0 +1,36 @@
+from __future__ import division, print_function
+
+import os
+import sys
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    from numpy.distutils.system_info import get_info
+    config = Configuration('utilities', parent_package, top_path)
+
+    config.add_data_dir('test')
+
+
+    config.add_extension('sparse_ext',
+                         sources='sparse_ext.c')
+
+    config.add_extension('sparse_matrix_ext',
+                         sources='sparse_matrix_ext.c')
+
+
+    config.add_extension('util_ext',
+                         sources='util_ext.c')
+
+    config.add_extension('cg_ext',
+                         sources='cg_ext.c',
+                         extra_compile_args=['-fopenmp'])
+
+    config.add_extension('quad_tree_ext',
+                         sources=['quad_tree_ext.c', 'quad_tree.c'])
+    
+
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(configuration=configuration)
