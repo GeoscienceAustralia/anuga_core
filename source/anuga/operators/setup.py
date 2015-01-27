6f87510f427baa986a6717d7d726204ec400629a
steve <stephen.roberts@anu.edu.au>
2015-01-27 22:58:35 +1100

    Added more setup.py files

--- source/anuga/operators/setup.py
+++ source/anuga/operators/setup.py
@@ -10,11 +10,20 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     from numpy.distutils.system_info import get_info
     
-    config = Configuration('alpha_shape', parent_package, top_path)
+    config = Configuration('operators', parent_package, top_path)
 
     config.add_data_dir('test')
 
     util_dir = os.path.abspath('../utilities')
+
+    config.add_extension('mannings_operator_ext',
+                         sources=['mannings_operator_ext.c'],
+                         include_dirs=[util_dir])
+
+    config.add_extension('kinematic_viscosity_operator_ext',
+                         sources=['kinematic_viscosity_operator_ext.c'],
+                         include_dirs=[util_dir])
+
     
     return config
     
---
+++ source/anuga/shallow_water/setup.py
@@ -0,0 +1,36 @@
+from __future__ import division, print_function
+
+import os
+import sys
+
+from os.path import join
+
+def configuration(parent_package='',top_path=None):
+    
+    from numpy.distutils.misc_util import Configuration
+    from numpy.distutils.system_info import get_info
+    
+    config = Configuration('shallow_water', parent_package, top_path)
+
+    config.add_data_dir('test')
+
+    util_dir = os.path.abspath('../utilities')
+    
+    config.add_extension('shallow_water_ext',
+                         sources=['shallow_water_ext.c'],
+                         include_dirs=[util_dir])
+    
+    config.add_extension('swb2_domain_ext',
+                         sources=['swb2_domain_ext.c'],
+                         include_dirs=[util_dir])
+
+    config.add_extension('swDE1_domain_ext',
+                         sources=['swDE1_domain_ext.c'],
+                         include_dirs=[util_dir])
+
+
+    return config
+    
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(configuration=configuration)
