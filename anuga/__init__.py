df43e634943f8525b21f0e3a5572133c18af42fa
steve <stephen.roberts@anu.edu.au>
2015-02-11 15:53:23 +1100

    Cleanup anuga/__init__.py file

--- anuga/__init__.py
+++ anuga/__init__.py
@@ -38,7 +38,7 @@
 #
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 #
-__version__ = '1.3.5'
+__version__ = '1.3.6'
 
 __svn_revision__ = filter(str.isdigit, "$Revision$")
 
