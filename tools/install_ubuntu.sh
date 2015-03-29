c654649f69535a7a4bb440966dcf53b2899be9bb
Stephen Roberts <stephen.roberts@anu.edu.au>
2015-03-30 10:39:23 +1100

    Remove sudo from install_ubuntu.sh
    Removing sudo from Travis CI install script

--- tools/install_ubuntu.sh
+++ tools/install_ubuntu.sh
@@ -12,27 +12,27 @@ set -e
 
 [ -z "$PYTHON_VERSION" ] && PYTHON_VERSION="2.7"
 
-sudo apt-get update -q
+apt-get update -q
 
 ##########################################################
 # Use standard ubuntu packages in their default version
     
-sudo apt-get install -q -y git gfortran python-dev python-numpy \
+apt-get install -q -y git gfortran python-dev python-numpy \
                              python-scipy \
                              python-matplotlib netcdf-bin \
                              libnetcdf-dev libhdf5-serial-dev \
                              python-gdal python-pip 
 
-sudo pip install nose netCDF4 pyproj
+pip install nose netCDF4 pyproj
     
 ##########################################################
 # Setup for various versions of MPI
 if [[ "$ANUGA_PARALLEL" == "mpich2" ]]; then
-    sudo apt-get install -y mpich2;
+    apt-get install -y mpich2;
 fi
 
 if [[ "$ANUGA_PARALLEL" == "openmpi" ]]; then
-    sudo apt-get install -y libopenmpi-dev openmpi-bin;
+    apt-get install -y libopenmpi-dev openmpi-bin;
 fi
 
 # Install pypar if parallel set
@@ -40,7 +40,7 @@ if [[ "$ANUGA_PARALLEL" == "mpich2" || "$ANUGA_PARALLEL" == "openmpi" ]]; then
      git clone https://github.com/daleroberts/pypar.git;
      pushd pypar;
      python setup.py build;
-     sudo python setup.py install;
+     python setup.py install;
      popd;
 fi
 
@@ -53,6 +53,6 @@ fi
 # build and install anuga
 
 python setup.py build
-sudo python setup.py install
+python setup.py install
 
 
