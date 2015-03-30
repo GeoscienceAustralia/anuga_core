1982812e3e0909daea619598d5e197b4b63cb2cc
Stephen Roberts <stephen.roberts@anu.edu.au>
2015-03-30 13:34:24 +1100

    Removed -Ofast flag

--- anuga/shallow_water/setup.py
+++ anuga/shallow_water/setup.py
@@ -28,8 +28,7 @@ def configuration(parent_package='',top_path=None):
 
     config.add_extension('swDE1_domain_ext',
                          sources=['swDE1_domain_ext.c'],
-                         include_dirs=[util_dir],
-                         extra_compile_args=['-Ofast'])
+                         include_dirs=[util_dir])
 
 
     return config
