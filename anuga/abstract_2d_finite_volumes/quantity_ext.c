d99c58fb5c0917447506e87e10557961b47ed825
Stephen Roberts <stephen.roberts@anu.edu.au>
2015-05-18 17:52:41 +1000

    playing with update

--- anuga/abstract_2d_finite_volumes/quantity_ext.c
+++ anuga/abstract_2d_finite_volumes/quantity_ext.c
@@ -813,6 +813,24 @@ int _update(int N,
 
 
 
+//	// Divide semi_implicit update by conserved quantity
+//	for (k=0; k<N; k++) {
+//		x = centroid_values[k];
+//		if (x == 0.0) {
+//			semi_implicit_update[k] = 0.0;
+//		} else {
+//			semi_implicit_update[k] /= x;
+//		}
+//		centroid_values[k] += timestep*explicit_update[k];
+//		denominator = 1.0 - timestep*semi_implicit_update[k];
+//		if (denominator <= 0.0) {
+//			return -1;
+//		} else {
+//			//Update conserved_quantities from semi implicit updates
+//			centroid_values[k] /= denominator;
+//		}
+//
+//	}
 
 
 	// Reset semi_implicit_update here ready for next time step
@@ -963,7 +981,7 @@ PyObject *update(PyObject *self, PyObject *args) {
 
 	if (err != 0) {
 	  PyErr_SetString(PyExc_RuntimeError,
-			  "quantity_ext.c: update, divsion by zero in semi implicit update - call Stephen :)");
+			  "quantity_ext.c: update, division by zero in semi implicit update - call Stephen :)");
 	  return NULL;
 	}
 
