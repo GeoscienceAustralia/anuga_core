f2bab168a05103de581cc9cc0a5cc0a8a1504a7e
Mariela Perignon <mariela@mac1-16-220-dhcp.int.colorado.edu>
2016-01-28 19:36:53 -0700

    example run files

--- examples/sed_transport/run_no_pole.py
+++ examples/sed_transport/run_no_pole.py
@@ -25,23 +25,37 @@ import numpy as num
 #------------------------------------------------------------------------------
 def topography(x,y):
     """Complex topography defined by a function of vectors x and y."""
-    print ' Create topography....'
     z = 10.-x/50
- 
-    z[y < 0.2] = 20.
-    z[y > width - 0.2] = 20.
+ # 
+#     z[y < 0.2] = 13.
+#     z[y > width - 0.2] = 13.
+    
+#     N = len(x)
+#     for i in range(N):
+#         # Step
+# #         if 2 < x[i] < 4:
+# #             z[i] += 0.4 - 0.05*y[i]
+# 
+#         # Permanent pole
+#         if (x[i] - 2.5)**2 + (y[i] - 2.5)**2 < 0.3**2:
+#             z[i] += 5
+#         
+#         if (x[i] - 3)**2 + (y[i] - 1.5)**2 < 0.4**2:
+#             z[i] += 5
+#             
+#         if (x[i] - 6)**2 + (y[i] - 2.5)**2 < 0.6**2:
+#             z[i] += 5
+#             
+#         if (x[i] - 10)**2 + (y[i] - 2)**2 < 0.5**2:
+#             z[i] += 5
             
     return z
 
 def depth(x,y):
     """Complex topography defined by a function of vectors x and y."""
-    print ' Create topography....'
-    z = 10.-x/50
- 
-    z[y < 0.2] = 20.
-    z[y > width - 0.2] = 20.
+    z = topography(x,y)
             
-    return z + 0.5
+    return z + 0.1
 
 #===============================================================================
 # Setup and Run Model
@@ -51,10 +65,9 @@ def depth(x,y):
 #------------------------------------------------------------------------------
 # Setup computational domain
 #------------------------------------------------------------------------------
-print ' Set up Domain first...'
 length = 15.
 width = 4.
-dx = dy = 0.2 #.1           # Resolution: Length of subdivisions on both axes
+dx = dy = 0.1 #.1           # Resolution: Length of subdivisions on both axes
 
 points, vertices, boundary = rectangular_cross(int(length/dx), int(width/dy),
                                                len1=length, len2=width)
@@ -67,7 +80,7 @@ domain.set_name('no_pole') # Output name
 # domain.set_store_vertices_uniquely(True)
 
 
-print domain.statistics()
+# print domain.statistics()
 
 domain.set_quantities_to_be_stored({'elevation': 2,
                                     'stage': 2,# 
@@ -75,10 +88,10 @@ domain.set_quantities_to_be_stored({'elevation': 2,
 #                                     'ymomentum': 2,
                                     'concentration': 2})
 
-# domain.set_quantity('concentration', 0.01)
+domain.set_quantity('concentration', 0.01)
 domain.set_quantity('elevation', topography)           # elevation is a function
 domain.set_quantity('friction', 0.01)                  # Constant friction
-domain.set_quantity('stage', depth)   # Dry initial condition
+domain.set_quantity('stage', topography)   # Dry initial condition
 
 #------------------------------------------------------------------------------
 # Setup boundary conditions
@@ -99,12 +112,13 @@ from anuga.operators.sed_transport_operator import Sed_transport_operator
 
 # create operator
 op1 = Sed_transport_operator(domain)
+# op1.set_inflow_concentration(0.02)
 
 
 #------------------------------------------------------------------------------
 # Evolve system through time
 #------------------------------------------------------------------------------
-for t in domain.evolve(yieldstep=0.5, finaltime=2.):
+for t in domain.evolve(yieldstep=0.5, finaltime=20.):
     domain.print_timestepping_statistics()
     #domain.print_operator_timestepping_statistics()
 
