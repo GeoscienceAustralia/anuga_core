d1562728235aba1613edc4b90bfb6931eba7aa09
Stephen Roberts <stephen.roberts@anu.edu.au>
2016-04-01 10:21:51 +1100

    testing discontinuous smoothed output

--- validation_tests/experimental_data/okushiri/run_okushiri.py
+++ validation_tests/experimental_data/okushiri/run_okushiri.py
@@ -81,8 +81,7 @@ if myid == 0:
     #-------------------------
     domain.set_name(project.output_filename)  # Name of output sww file 
     domain.set_minimum_storable_height(0.01) # Don't store w < 0.01m
-    #domain.set_quantities_to_be_monitored('stage')
-
+    domain.set_store_vertices_smoothly(True)
     domain.set_flow_algorithm(alg)
 
 else:
