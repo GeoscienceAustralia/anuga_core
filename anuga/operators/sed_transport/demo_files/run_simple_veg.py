0c61f627b666c09dca7588e051dcb2867fa7fc92
Mariela Perignon <mariela@mac1-16-220-dhcp.int.colorado.edu>
2016-04-08 16:53:06 -0600

    finish sed in doc, start veg

--- anuga/operators/sed_transport/demo_files/run_simple_veg.py
+++ anuga/operators/sed_transport/demo_files/run_simple_veg.py
@@ -1,41 +1,36 @@
-"""Simple water flow example using ANUGA
+"""
 
-Water flowing down a channel with a topography that varies with time
+Example of use of sediment transport and vegetation drag operators with simple water flow down a channel
+
+M. Perignon
+perignon@colorado.edu
+April 2016
+
 """
 
 #------------------------------------------------------------------------------
 # Import necessary modules
 #------------------------------------------------------------------------------
+import anuga
 from anuga import rectangular_cross
 from anuga import Domain
-from anuga import Reflective_boundary
 from anuga import Dirichlet_boundary
-from anuga import Transmissive_boundary
-from anuga import Time_boundary
+from anuga import Reflective_boundary
 
-from anuga import Quantity
-
-import numpy as num
 
 #===============================================================================
 # Setup Functions
 #===============================================================================
 
-
 #------------------------------------------------------------------------------
 # Setup initial conditions
 #------------------------------------------------------------------------------
 def topography(x,y):
-    """Complex topography defined by a function of vectors x and y."""
-    z = 10.-x/50
+    z = -x/100
     
     return z
 
-def depth(x,y):
-    """Complex topography defined by a function of vectors x and y."""
-    z = topography(x,y)
-            
-    return z + 0.1
+
 
 #===============================================================================
 # Setup and Run Model
@@ -45,52 +40,57 @@ def depth(x,y):
 #------------------------------------------------------------------------------
 # Setup computational domain
 #------------------------------------------------------------------------------
-length = 15.
-width = 4.
-dx = dy = 0.25 #.1           # Resolution: Length of subdivisions on both axes
+length = 5.
+width = 5.
+dx = dy = 2
+
+
+"""
+Must include the process-specific quantities when creating the domain
+"""        
 
 points, vertices, boundary = rectangular_cross(int(length/dx), int(width/dy),
                                                len1=length, len2=width)
+   
+domain = Domain(points, vertices, boundary)
 
-evolved_quantities = ['stage', 'xmomentum', 'ymomentum', 'elevation','concentration']
-                                               
-domain = Domain(points, vertices, boundary, evolved_quantities=evolved_quantities)
+
+
 domain.set_flow_algorithm('DE0')
-domain.set_name('veg_sed') # Output name
-# domain.set_store_vertices_uniquely(True)
+domain.set_name('run_simple_veg') # Output name
+domain.set_store_vertices_uniquely(True)
+domain.set_quantity('elevation', topography)           # elevation is a function
+domain.set_quantity('stage', expression='elevation')   # Dry initial condition
 
-# print domain.statistics()
 
+"""
+Store process-specific quantities with same functions
+""" 
 domain.set_quantities_to_be_stored({'elevation': 2,
-                                    'stage': 2, 
+                                    'stage': 2,
                                     'xmomentum': 2,
                                     'ymomentum': 2,
                                     'concentration': 2})
-
-
-domain.set_quantity('elevation', topography)           # elevation is a function
-domain.set_quantity('friction', 0.01)                  # Constant friction
-domain.set_quantity('stage', topography)   # Dry initial condition
-
+                                    
 #------------------------------------------------------------------------------
 # Setup boundary conditions
 #------------------------------------------------------------------------------
-Bi = Dirichlet_boundary([11.5, 0, 0])          # Inflow
-Br = Reflective_boundary(domain)              # Solid reflective wall
-# Bo = Dirichlet_boundary([0, 0., 0.])           # Outflow
-Bo = Transmissive_boundary(domain)
+max_elev = domain.quantities['elevation'].vertex_values.max()
+min_elev = domain.quantities['elevation'].vertex_values.min()
+
+
+Bi = Dirichlet_boundary([max_elev + 0.5, 0, 0])
+Br = Reflective_boundary(domain)           # Solid reflective wall
+Bo = Dirichlet_boundary([min_elev - 1, 0, 0])    # Outflow
 
 domain.set_boundary({'left': Bi, 'right': Bo, 'top': Br, 'bottom': Br})
 
 #------------------------------------------------------------------------------
-# Setup erosion operator in the middle of dam
+# Setup operators
 #------------------------------------------------------------------------------
-print 'Set up vegetation to test...'
-
 
 from anuga.operators.vegetation_operator import Vegetation_operator
 
-# create operator
 op1 = Vegetation_operator(domain)
 
 Quantity(domain, name='veg_diameter', register=True)
@@ -99,21 +99,11 @@ domain.set_quantity('veg_diameter', 0.00064)
 Quantity(domain, name='veg_spacing', register=True)
 domain.set_quantity('veg_spacing', 0.15)
 
-
-from anuga.operators.sed_transport_operator import Sed_transport_operator
-
-# create operator
-op1 = Sed_transport_operator(domain)
-
-# Quantity(domain, name='concentration', register=True)
-domain.set_quantity('concentration', 0.01)
-
 #------------------------------------------------------------------------------
 # Evolve system through time
 #------------------------------------------------------------------------------
-for t in domain.evolve(yieldstep=0.5, finaltime=30.):
+for t in domain.evolve(yieldstep = 1, finaltime = 30.0):
     domain.print_timestepping_statistics()
-    #domain.print_operator_timestepping_statistics()
 
 
 
