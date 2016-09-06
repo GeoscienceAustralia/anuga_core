2110875eb603a790317367dec77566dad7f3e670
Stephen Roberts <stephen.roberts@anu.edu.au>
2016-09-06 12:21:51 +1000

    Change install script to separate dependencies and install

---
+++ tools/install_ubuntu_dependencies.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# This script is meant to be called by the "install" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
+#
+# But can also be used to install anuga dependencies
+
+# License: 3-clause BSD
+
+
+set -e
+
+PYTHON_VERSION=${PYTHON_VERSION:-"2.7"}
+ANUGA_PARALLEL=${ANUGA_PARALLEL:-"openmpi"}
+
+sudo apt-get update -q
+
+##########################################################
+# Use standard ubuntu packages in their default version
+    
+sudo apt-get install -q -y git gfortran python-dev python-numpy \
+                             python-scipy \
+                             python-matplotlib netcdf-bin \
+                             libnetcdf-dev libhdf5-serial-dev \
+                             python-gdal gdal-bin python-pip 
+
+
+sudo pip install nose netCDF4 pyproj
+    
+##########################################################
+# Setup for various versions of MPI
+if [[ "$ANUGA_PARALLEL" == "mpich2" ]]; then
+    sudo apt-get install -y mpich2;
+fi
+
+if [[ "$ANUGA_PARALLEL" == "openmpi" ]]; then
+    sudo apt-get install -y libopenmpi-dev openmpi-bin;
+fi
+
+# Install pypar if parallel set
+if [[ "$ANUGA_PARALLEL" == "mpich2" || "$ANUGA_PARALLEL" == "openmpi" ]]; then
+     git clone https://github.com/daleroberts/pypar.git;
+     pushd pypar;
+     python setup.py build;
+     sudo python setup.py install;
+     popd;
+fi
+
+
+
+
