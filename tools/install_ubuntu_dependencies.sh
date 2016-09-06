266106786583bd125ac5dfaffcb279a6acabc24d
Stephen Roberts <stephen.roberts@anu.edu.au>
2016-09-06 13:30:28 +1000

    default scalar multiplication caused error

--- tools/install_ubuntu_dependencies.sh
+++ tools/install_ubuntu_dependencies.sh
@@ -14,28 +14,28 @@ set -e
 PYTHON_VERSION=${PYTHON_VERSION:-"2.7"}
 ANUGA_PARALLEL=${ANUGA_PARALLEL:-"openmpi"}
 
-sudo apt-get update -q
+apt-get update -q
 
 ##########################################################
 # Use standard ubuntu packages in their default version
     
-sudo apt-get install -q -y git gfortran python-dev python-numpy \
+apt-get install -q -y git gfortran python-dev python-numpy \
                              python-scipy \
                              python-matplotlib netcdf-bin \
                              libnetcdf-dev libhdf5-serial-dev \
                              python-gdal gdal-bin python-pip 
 
 
-sudo pip install nose netCDF4 pyproj
+pip install nose netCDF4 pyproj
     
 ##########################################################
 # Setup for various versions of MPI
 if [[ "$ANUGA_PARALLEL" == "mpich2" ]]; then
-    sudo apt-get install -y mpich2;
+    apt-get install -y mpich2;
 fi
 
 if [[ "$ANUGA_PARALLEL" == "openmpi" ]]; then
-    sudo apt-get install -y libopenmpi-dev openmpi-bin;
+    apt-get install -y libopenmpi-dev openmpi-bin;
 fi
 
 # Install pypar if parallel set
