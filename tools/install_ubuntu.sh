4cb2431377653e66e179a01666db9a2c32a293f9
Stephen Roberts <stephen.roberts@anu.edu.au>
2016-09-09 14:34:58 +1000

    only parallel install if env set

--- tools/install_ubuntu.sh
+++ tools/install_ubuntu.sh
@@ -13,18 +13,19 @@ PYTHON_VERSION=${PYTHON_VERSION:-"2.7"}
 ANUGA_PARALLEL=${ANUGA_PARALLEL:-"false"}
 
 ###########################################################
+# Check if mpich2 has been installed
+if [ $(dpkg-query -W -f='${Status}\n' mpich2 2>/dev/null | grep -c "ok installed") -gt 0 ];
+then
+  ANUGA_PARALLEL="mpich2"
+fi
+
+###########################################################
 # Check if openmpi has been installed
-if [ $(dpkg-query -W -f='${Status}' openmpi-bin 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dpkg-query -W -f='${Status}\n' openmpi-bin 2>/dev/null | grep -c "ok installed") -gt 0 ];
 then
   ANUGA_PARALLEL="openmpi"
 fi
 
-###########################################################
-# Check if mpich2 has been installed
-if [ $(dpkg-query -W -f='${Status}' mpich2 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  ANUGA_PARALLEL="mpich2"
-fi
 
 
 sudo apt-get update -q
