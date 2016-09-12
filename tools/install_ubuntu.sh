6d0c928507a1a977d2c8b9bec5055ea5b46c6397
Stephen Roberts <stephen.roberts@anu.edu.au>
2016-09-12 13:01:06 +1000

    Install with parallel default

--- tools/install_ubuntu.sh
+++ tools/install_ubuntu.sh
@@ -14,14 +14,14 @@ ANUGA_PARALLEL=${ANUGA_PARALLEL:-"false"}
 
 ###########################################################
 # Check if openmpi has been installed
-if [ $(dpkg-query -W -f='${Status}' openmpi-bin 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dpkg-query -W -f='${Status}' openmpi-bin 2>/dev/null | grep -c "ok installed") -gt 0 ];
 then
   ANUGA_PARALLEL="openmpi"
 fi
 
 ###########################################################
 # Check if mpich2 has been installed
-if [ $(dpkg-query -W -f='${Status}' mpich2 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dpkg-query -W -f='${Status}' mpich2 2>/dev/null | grep -c "ok installed") -gt 0 ];
 then
   ANUGA_PARALLEL="mpich2"
 fi
@@ -31,6 +31,10 @@ sudo apt-get update -q
 
 ##########################################################
 # Use standard ubuntu packages in their default version
+
+echo "+===============================================+"
+echo "|  Using apt-get to install standard packages   |"
+echo "+===============================================+"
     
 sudo apt-get install -q -y git gfortran python-dev python-numpy \
                              python-scipy \
@@ -39,20 +43,33 @@ sudo apt-get install -q -y git gfortran python-dev python-numpy \
                              python-gdal gdal-bin python-pip 
 
 
+echo "+===============================================+"
+echo "|  Using pip to install standard packages       |"
+echo "+===============================================+"
+
 sudo pip install nose netCDF4 pyproj
     
 ##########################################################
 # Setup for various versions of MPI
 if [[ "$ANUGA_PARALLEL" == "mpich2" ]]; then
+    echo "+===============================================+"
+    echo "|  Using apt-get to install mpich package       |"
+    echo "+===============================================+"
     sudo apt-get install -y mpich2;
 fi
 
 if [[ "$ANUGA_PARALLEL" == "openmpi" ]]; then
+    echo "+===============================================+"
+    echo "|  Using apt-get to install openmpi package       |"
+    echo "+===============================================+"
     sudo apt-get install -y libopenmpi-dev openmpi-bin;
 fi
 
 # Install pypar if parallel set
 if [[ "$ANUGA_PARALLEL" == "mpich2" || "$ANUGA_PARALLEL" == "openmpi" ]]; then
+    echo "+===============================================+"
+    echo "|  Installing pypar from source                 |"
+    echo "+===============================================+"
      git clone https://github.com/daleroberts/pypar.git;
      pushd pypar;
      python setup.py build;
@@ -63,6 +80,9 @@ fi
 #########################################################
 # Build and install anuga
 
+echo "+===============================================+"
+echo "|  Using setup.py to install anuga              |"
+echo "+===============================================+"
 python setup.py build
 sudo python setup.py install 
 
