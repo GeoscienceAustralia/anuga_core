f6d416883a820abcbb351b00726c6f33ca9919a7
Stephen Roberts <stephen.roberts@anu.edu.au>
2017-05-16 08:42:23 +1000

    Update install_ubuntu.sh
    Problem with conflict of gdal libs in latest version of 12.04 on travis. Trying work around https://www.mail-archive.com/ubuntu@lists.osgeo.org/msg00885.html

--- tools/install_ubuntu.sh
+++ tools/install_ubuntu.sh
@@ -35,8 +35,19 @@ then
 fi
 
 
+UBUNTU_VERSION = `lsb_release -r -s`
 
+if [[ "$UBUNTU_VERSION" == "12.04" ]]; then
+    echo "+===============================================+"
+    echo "|  Work around for installing gdal on 12.04     |"
+    echo "+===============================================+"
+    sudo mv /etc/apt/sources.list.d/pgdg-source.list* /tmp ;
+    sudo apt-get -qq remove postgis;
+    sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable;
+fi    
+    
 sudo apt-get update -q
+
 
 ##########################################################
 # Use standard ubuntu packages in their default version
