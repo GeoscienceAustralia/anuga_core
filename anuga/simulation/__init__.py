bfdec826b802bc3b8da9b297b76664f6a1cc0c09
Stephen Roberts <stephen.roberts@anu.edu.au>
2018-09-04 20:24:28 +1000

    setup simulation module

---
+++ anuga/simulation/__init__.py
@@ -0,0 +1,9 @@
+""" Simulation Module
+
+    This module wraps the standard anuga script into a simulation class which
+    allows for the user to define the standard creation of domain etc
+
+"""
+
+from numpy.testing import Tester
+test = Tester().test
