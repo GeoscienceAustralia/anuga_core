5e5f91cda5af80eb4d58f699ec967b5ad5b3ec16
rkwi <kenny.wiratama@gmail.com>
2019-11-05 16:56:49 +1100

    Cythonizing neighbour_table

--- anuga/abstract_2d_finite_volumes/setup.py
+++ anuga/abstract_2d_finite_volumes/setup.py
@@ -30,7 +30,7 @@ def configuration(parent_package='',top_path=None):
                      	include_dirs=[util_dir])
 
  	config.add_extension('neighbour_table_ext',
-                       	sources=['neighbour_table_ext.cpp'],
+                       	sources=['neighbour_table_interface.pyx'],
                        	extra_compile_args=["-std=c++11"],
                        	language='c++',
                        	include_dirs=[util_dir])
