b9275e35df116296929edd6d47f9a3a5ee971f67
Stephen Roberts <stephen.roberts@anu.edu.au>
2019-11-24 08:31:12 +1100

    utility to move from master to anuga_py3 branches

---
+++ tools/clear_for_master.sh
@@ -0,0 +1,10 @@
+rm ../anuga/abstract_2d_finite_volumes/mesh_factory_ext.c
+rm ../anuga/abstract_2d_finite_volumes/neighbour_mesh_ext.c
+rm ../anuga/abstract_2d_finite_volumes/neighbour_table_ext.cpp
+rm ../anuga/abstract_2d_finite_volumes/pmesh2domain_ext.c
+rm ../anuga/abstract_2d_finite_volumes/quantity_ext.c
+rm ../anuga/advection/advection_ext.c
+rm ../anuga/shallow_water/shallow_water_ext.c
+rm ../anuga/shallow_water/swDE1_domain_ext.c
+rm ../anuga/shallow_water/swb2_domain_ext.c
+
