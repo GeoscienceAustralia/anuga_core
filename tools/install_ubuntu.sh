71c9d9f2006fb63a013652f0f9cb38be3f89b573
Stephen Roberts <stephen.roberts@anu.edu.au>
2019-12-16 16:56:48 +1100

    added a space to if statement

--- tools/install_ubuntu.sh
+++ tools/install_ubuntu.sh
@@ -11,7 +11,7 @@ set -e
 
 PYTHON_VERSION=${PYTHON_VERSION:-"2.7"}
 ANUGA_PARALLEL=${ANUGA_PARALLEL:-"false"}
-if [[ "$ANUGA_PARALLEL" == "false"]];
+if [[ "$ANUGA_PARALLEL" == "false" ]];
 then 
     PYPAR_AVAILABLE="false"
 else
@@ -123,7 +123,7 @@ if [[ "$PYPAR_AVAILABLE" == "pypar" ]]; then
 fi
 
 
-if [[ "$PYPAR_AVAILABLE" == "mpi4py"]]; then 
+if [[ "$PYPAR_AVAILABLE" == "mpi4py" ]]; then 
     echo "+===============================================+"
     echo "|  Using pip to install mpi4py                  |"
     echo "+===============================================+"
