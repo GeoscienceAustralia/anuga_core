7432e47bf3484e7531f7a9f7e2132a86840496e3
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2020-06-03 11:08:41 +1000

    One more - looks like all validations (7) are running now

--- validation_tests/analytical_exact/carrier_greenspan_periodic/error.py
+++ validation_tests/analytical_exact/carrier_greenspan_periodic/error.py
@@ -1,12 +1,13 @@
 ## module error
-''' err(string).
+""" err(string).
     Prints 'string' and terminates program.
 
     Taken from the book Numerical Methods in Engineering with Python
     by J. Kiusalaas    
-'''    
+"""
+# FIXME (Ole): Use exception handling instead of this
 import sys
 def err(string):
-    print string
+    print(string)
     raw_input('Press return to exit')
     sys.exit()
