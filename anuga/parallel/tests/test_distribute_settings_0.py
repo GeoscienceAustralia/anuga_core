44a04a2f9b11518c5d354d44a72b23ce5b677f6c
Stephen Roberts <stoiver@gmail.com>
2020-06-04 18:49:47 +1000

    getting test distribute setting

--- anuga/parallel/tests/test_distribute_settings_0.py
+++ anuga/parallel/tests/test_distribute_settings_0.py
@@ -29,114 +29,152 @@ from anuga import Dirichlet_boundary
 from anuga import Time_boundary
 from anuga import Transmissive_boundary
 from anuga import rectangular_cross_domain
-from anuga import Geo_reference
 
 from anuga import distribute, myid, numprocs, send, receive, barrier, finalize
-
-from anuga.parallel.sequential_distribute import sequential_distribute_dump
-from anuga.parallel.sequential_distribute import sequential_distribute_load
-
-import anuga.utilities.plot_utils as util
 
 #--------------------------------------------------------------------------
 # Setup parameters
 #--------------------------------------------------------------------------
 yieldstep = 0.25
-finaltime = 3.0
+finaltime = 1.0
 nprocs = 4
 N = 29
-M = 29 
-verbose = True
-
-
-new_parameters = {}
-new_parameters['ghost_layer_width'] = 2
+M = 29
+verbose = False
 
 #---------------------------------
 # Setup Functions
 #---------------------------------
-def topography(x,y): 
-    return -x/2.0
+def topography(x,y):
+    return old_div(-x,2)
 
 ###########################################################################
 # Setup Test
 ##########################################################################
-def run_simulation(parallel=False, verbose=False):
+def run_simulation(parallel=False, G = None, seq_interpolation_points=None, verbose=False):
 
     #--------------------------------------------------------------------------
     # Setup computational domain and quantities
     #--------------------------------------------------------------------------
     domain = rectangular_cross_domain(M, N)
-    domain.set_name('odomain')                    # Set sww filename
-    domain.set_datadir('.')   
+
+
     domain.set_quantity('elevation', topography) # Use function for elevation
-    domain.set_quantity('friction', 0.0)         # Constant friction 
+    domain.set_quantity('friction', 0.0)         # Constant friction
     domain.set_quantity('stage', expression='elevation') # Dry initial stage
-    
-    domain.set_quantities_to_be_stored({'elevation': 2,'stage': 2,'xmomentum': 2,'ymomentum': 2})
-    domain.set_store_vertices_uniquely(False)
-    domain.set_flow_algorithm('DE1')
-    georef = Geo_reference(zone=56,xllcorner=100000.0,yllcorner=200000.0)
-    domain.set_georeference(georef)
-        
-    #--------------------------------------------------------------------------
-    # Create pickled partition
-    #--------------------------------------------------------------------------
-    if myid == 0:
-        if verbose: print('DUMPING PARTITION DATA')
-        sequential_distribute_dump(domain, numprocs, verbose=verbose, parameters=new_parameters)    
+
+    domain.set_low_froude(0)
+
+    domain.set_name('runup')                    # Set sww filename
+    domain.set_datadir('.')                     # Set output dir
 
     #--------------------------------------------------------------------------
-    # Create the parallel domains
+    # Create the parallel domain
     #--------------------------------------------------------------------------
     if parallel:
-        
-        if myid == 0 and verbose : print('DISTRIBUTING TO PARALLEL DOMAIN')
-        pdomain = distribute(domain, verbose=verbose, parameters=new_parameters)
-        pdomain.set_name('pdomain')
-        
-        if myid == 0 and verbose : print('LOADING IN PARALLEL DOMAIN')
-        sdomain = sequential_distribute_load(filename='odomain', verbose = verbose)
-        sdomain.set_name('sdomain')
-        
-        if myid == 0 and verbose : print('TESTING AGAINST SEQUENTIAL DOMAIN')
-        assert domain.get_datadir() == pdomain.get_datadir()
-        assert domain.get_store() == pdomain.get_store()
-        assert domain.get_store_centroids() == pdomain.get_store_centroids()
-        assert domain.smooth == pdomain.smooth
-        assert domain.reduction == pdomain.reduction
-        assert domain.minimum_storable_height == pdomain.minimum_storable_height
-        assert domain.get_flow_algorithm() == pdomain.get_flow_algorithm()
-        assert domain.get_minimum_allowed_height() == pdomain.get_minimum_allowed_height()
-        assert domain.geo_reference == pdomain.geo_reference
-        
-        assert domain.get_datadir() == sdomain.get_datadir()
-        assert domain.get_store() == sdomain.get_store()
-        assert domain.get_store_centroids() == sdomain.get_store_centroids()
-        assert domain.smooth == sdomain.smooth
-        assert domain.reduction == sdomain.reduction
-        assert domain.minimum_storable_height == sdomain.minimum_storable_height
-        assert domain.get_flow_algorithm() == sdomain.get_flow_algorithm()
-        assert domain.get_minimum_allowed_height() == sdomain.get_minimum_allowed_height()
-        assert domain.geo_reference == sdomain.geo_reference
+        if myid == 0 and verbose : print('DISTRIBUTING PARALLEL DOMAIN')
+        domain = distribute(domain, verbose=False)
 
-        if myid == 0 and verbose : print('REMOVING DATA FILES')
-        if myid == 0:
-            import os
-            #os.remove('odomain.sww')
-            #os.remove('pdomain.sww')
-            #os.remove('sdomain.sww')
-            try:
-                os.remove('odomain_P4_0.pickle')
-                os.remove('odomain_P4_1.pickle')
-                os.remove('odomain_P4_2.pickle')
-                os.remove('odomain_P4_3.pickle')
-                import glob
-                [ os.remove(fl) for fl in glob.glob('*.npy') ]
-            except: 
-                if verbose: print('remove files failed')
+    #--------------------------------------------------------------------------
+    # Setup domain parameters
+    #--------------------------------------------------------------------------
 
-        if myid == 0 and verbose : print('FINISHED')
+
+    domain.set_quantities_to_be_stored(None)
+
+
+    #------------------------------------------------------------------------------
+    # Setup boundary conditions
+    # This must currently happen *AFTER* domain has been distributed
+    #------------------------------------------------------------------------------
+
+    Br = Reflective_boundary(domain)      # Solid reflective wall
+    Bd = Dirichlet_boundary([-0.2,0.,0.]) # Constant boundary values
+
+    # Associate boundary tags with boundary objects
+    domain.set_boundary({'left': Br, 'right': Bd, 'top': Br, 'bottom': Br})
+
+    #------------------------------------------------------------------------------
+    # Find which sub_domain in which the interpolation points are located
+    #
+    # Sometimes the interpolation points sit exactly
+    # between two centroids, so in the parallel run we
+    # reset the interpolation points to the centroids
+    # found in the sequential run
+    #------------------------------------------------------------------------------
+    interpolation_points = [[0.4,0.5], [0.6,0.5], [0.8,0.5], [0.9,0.5]]
+
+
+    gauge_values = []
+    tri_ids = []
+    for i, point in enumerate(interpolation_points):
+        gauge_values.append([]) # Empty list for timeseries
+
+        #if is_inside_polygon(point, domain.get_boundary_polygon()):
+        #print "Point ", myid, i, point
+        try:
+            k = domain.get_triangle_containing_point(point)
+            if domain.tri_full_flag[k] == 1:
+                tri_ids.append(k)
+            else:
+                tri_ids.append(-1)
+        except:
+            tri_ids.append(-2)
+
+        #print "  tri_ids ",myid, i, tri_ids[-1]
+
+    if verbose: print('P%d has points = %s' %(myid, tri_ids))
+
+
+    c_coord = domain.get_centroid_coordinates()
+    interpolation_points = []
+    for id in tri_ids:
+        if id<1:
+            if verbose: print('WARNING: Interpolation point not within the domain!')
+        interpolation_points.append(c_coord[id,:])
+
+    #------------------------------------------------------------------------------
+    # Evolve system through time
+    #------------------------------------------------------------------------------
+    time = []
+
+    if parallel:
+        if myid == 0 and verbose: print('PARALLEL EVOLVE')
+    else:
+        if myid == 0 and verbose: print('SEQUENTIAL EVOLVE')
+
+
+    for t in domain.evolve(yieldstep = yieldstep, finaltime = finaltime):
+        if myid == 0 and verbose : domain.write_time()
+
+        # Record time series at known points
+        time.append(domain.get_time())
+
+        stage = domain.get_quantity('stage')
+
+        for i in range(4):
+            if tri_ids[i] > -1:
+                gauge_values[i].append(stage.centroid_values[tri_ids[i]])
+
+
+    #----------------------------------------
+    # Setup test arrays during sequential run
+    #----------------------------------------
+    if not parallel:
+        G = []
+        for i in range(4):
+            G.append(gauge_values[i])
+
+    success = True
+
+    for i in range(4):
+        if tri_ids[i] > -1:
+            #print num.max(num.array(gauge_values[i])- num.array(G[i]))
+            success = success and num.allclose(gauge_values[i], G[i])
+
+    assert_(success)
+
+    return G, interpolation_points
 
 # Test an nprocs-way run of the shallow water equations
 # against the sequential code.
@@ -146,8 +184,7 @@ class Test_parallel_sw_flow(unittest.TestCase):
         if verbose : print("Expect this test to fail if not run from the parallel directory.")
 
         abs_script_name = os.path.abspath(__file__)
-        #cmd = "mpiexec -np %d  --oversubscribe python %s" % (3, abs_script_name)
-        cmd = "mpiexec -np 3 pwd"
+        cmd = "mpiexec -np %d  --oversubscribe python %s" % (3, abs_script_name)
         result = os.system(cmd)
 
         assert_(result == 0)
@@ -167,6 +204,19 @@ if __name__=="__main__":
     else:
 
         #------------------------------------------
+        # Run the sequential code on each processor
+        # and save results at 4 gauge stations to
+        # array G
+        #------------------------------------------
+        barrier()
+        if myid == 0 and verbose: print('SEQUENTIAL START')
+
+        G , interpolation_points = run_simulation(parallel=False,verbose=verbose)
+        G = num.array(G,num.float)
+
+        barrier()
+
+        #------------------------------------------
         # Run the code code and compare sequential
         # results at 4 gauge stations
         #------------------------------------------
@@ -176,6 +226,6 @@ if __name__=="__main__":
         import sys
         sys.excepthook = global_except_hook
 
-        run_simulation(parallel=True, verbose=verbose)
+        run_simulation(parallel=True, G=G, seq_interpolation_points = interpolation_points, verbose= verbose)
 
         finalize()
