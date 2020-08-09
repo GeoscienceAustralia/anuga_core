53d00acb182a8edb26ba27329e8777b758fa296c
Stephen Roberts <stoiver@gmail.com>
2020-08-09 16:48:02 +1000

    add latest revision.py

--- anuga/revision.py
+++
@@ -1,8 +0,0 @@
-"""Stored git revision info.
-
-This file provides the git sha id and commit date for the installed revision of ANUGA.
-The file is automatically generated and should not be modified manually.
-"""
-
-__git_sha__ = 'No git sha available'
-__git_committed_datetime__ = 'No git date available'
