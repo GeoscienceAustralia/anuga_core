d761afb6ce70ca11d6c3637ce2f223b173f8e441
Stephen Roberts <stoiver@gmail.com>
2020-09-03 16:46:30 +1000

    fixed up print statement

--- examples/structures/run_open_slot_wide_bridge.py
+++ examples/structures/run_open_slot_wide_bridge.py
@@ -10,79 +10,79 @@ Similarly the Outflow has MOMENTUM Not just Up welling as in the Horizontal Styl
 abstraction
 
 """
-print ('Starting.... Importing Modules...')
-#------------------------------------------------------------------------------
-# Import necessary modules
-#------------------------------------------------------------------------------
-import anuga
-
-from anuga.abstract_2d_finite_volumes.mesh_factory import rectangular_cross
-from anuga.shallow_water.shallow_water_domain import Domain
+from math import pi, pow, sqrt
+import numpy as num
+from anuga.culvert_flows.culvert_routines import boyd_generalised_culvert_model
+from anuga.culvert_flows.culvert_class import Culvert_flow
 from anuga.shallow_water.forcing import Rainfall, Inflow
+from anuga.shallow_water.shallow_water_domain import Domain
+from anuga.abstract_2d_finite_volumes.mesh_factory import rectangular_cross
+import anuga
+print('Starting.... Importing Modules...')
+# ------------------------------------------------------------------------------
+# Import necessary modules
+# ------------------------------------------------------------------------------
+
 #from anuga.shallow_water.forcing import Reflective_boundary
 #from anuga.shallow_water.forcing import Dirichlet_boundary
 #from anuga.shallow_water.forcing import Transmissive_boundary, Time_boundary
 
-from anuga.culvert_flows.culvert_class import Culvert_flow
-from anuga.culvert_flows.culvert_routines import boyd_generalised_culvert_model
 #from anuga.culvert_flows.culvert_routines import weir_orifice_channel_culvert_model
-from math import pi,pow,sqrt
-
-import numpy as num
 
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Setup computational domain
-#------------------------------------------------------------------------------
-print ('Setting up domain')
+# ------------------------------------------------------------------------------
+print('Setting up domain')
 
-length = 200. #x-Dir
-width = 200.  #y-dir
+length = 200.  # x-Dir
+width = 200.  # y-dir
 
 dx = dy = 2.5          # Resolution: Length of subdivisions on both axes
-#dx = dy = .5           # Resolution: Length of subdivisions on both axes
-#dx = dy = .5           # Resolution: Length of subdivisions on both axes
-#dx = dy = .1           # Resolution: Length of subdivisions on both axes
+# dx = dy = .5           # Resolution: Length of subdivisions on both axes
+# dx = dy = .5           # Resolution: Length of subdivisions on both axes
+# dx = dy = .1           # Resolution: Length of subdivisions on both axes
 
-points, vertices, boundary = rectangular_cross(int(length/dx), int(width/dy),
-                                               len1=length, len2=width)
-domain = Domain(points, vertices, boundary)   
+domain = rectangular_cross_domain(int(length/dx), int(width/dy),
+                                  len1=length, len2=width)
+
 domain.set_name('Test_open_slot_wide_bridge')                 # Output name
 # domain.set_default_order(2)
 # domain.H0 = 0.01
 # domain.tight_slope_limiters = 1
 
-print ('Size', len(domain))
+print('Size', len(domain))
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Setup initial conditions
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
 
 def topography(x, y):
     """Set up a weir
-    
+
     A culvert will connect either side
     """
     # General Slope of Topography
-    z=10.0-x/100.0  # % Longitudinal Slope
-    
+    z = 10.0-x/100.0  # % Longitudinal Slope
+
     #       NOW Add bits and Pieces to topography
-    bank_hgt=10.0
+    bank_hgt = 10.0
     bridge_width = 50.0
     bank_width = 10.0
-    
-    us_apron_skew = 1.0 # 1.0 = 1 Length: 1 Width, 2.0 = 2 Length : 1 Width
+
+    us_apron_skew = 1.0  # 1.0 = 1 Length: 1 Width, 2.0 = 2 Length : 1 Width
     us_start_x = 10.0
     top_start_y = 50.0
-    us_slope = 3.0  #Horiz : Vertic
+    us_slope = 3.0  # Horiz : Vertic
     ds_slope = 3.0
-    ds_apron_skew = 1.0 # 1.0 = 1 Length: 1 Width, 2.0 = 2 Length : 1 Width
-    centre_line_y= top_start_y+bridge_width/2.0
+    ds_apron_skew = 1.0  # 1.0 = 1 Length: 1 Width, 2.0 = 2 Length : 1 Width
+    centre_line_y = top_start_y+bridge_width/2.0
 
     # CALCULATE PARAMETERS TO FORM THE EMBANKMENT
     us_slope_length = bank_hgt*us_slope
-    us_end_x =us_start_x + us_slope_length
-    us_toe_start_y =top_start_y - us_slope_length / us_apron_skew
+    us_end_x = us_start_x + us_slope_length
+    us_toe_start_y = top_start_y - us_slope_length / us_apron_skew
     us_toe_end_y = top_start_y + bridge_width + us_slope_length / us_apron_skew
 
     top_end_y = top_start_y + bridge_width
@@ -90,53 +90,52 @@ def topography(x, y):
     ds_start_x = us_end_x + bank_width
     ds_end_x = ds_start_x + ds_slope_length
 
-    ds_toe_start_y =top_start_y - ds_slope_length / ds_apron_skew
+    ds_toe_start_y = top_start_y - ds_slope_length / ds_apron_skew
     ds_toe_end_y = top_start_y + bridge_width + ds_slope_length / ds_apron_skew
-
 
     N = len(x)
     for i in range(N):
 
        # Sloping Embankment Across Channel
-        if us_start_x < x[i] < us_end_x +0.1:   # For UPSLOPE on the Upstream FACE
-        #if 5.0 < x[i] < 10.1: # For a Range of X, and over a Range of Y based on X adjust Z
-            if us_toe_start_y +(x[i] - us_start_x)/us_apron_skew < y[i] < us_toe_end_y - (x[i] - us_start_x)/us_apron_skew:
-			#if  49.0+(x[i]-5.0)/5.0 <  y[i]  < 151.0 - (x[i]-5.0)/5.0: # Cut Out Base Segment for Culvert FACE
-               z[i]=z[i] # Flat Apron
+        if us_start_x < x[i] < us_end_x + 0.1:   # For UPSLOPE on the Upstream FACE
+            # if 5.0 < x[i] < 10.1: # For a Range of X, and over a Range of Y based on X adjust Z
+            if us_toe_start_y + (x[i] - us_start_x)/us_apron_skew < y[i] < us_toe_end_y - (x[i] - us_start_x)/us_apron_skew:
+                # if  49.0+(x[i]-5.0)/5.0 <  y[i]  < 151.0 - (x[i]-5.0)/5.0: # Cut Out Base Segment for Culvert FACE
+                z[i] = z[i]  # Flat Apron
             else:
-               z[i] += z[i] + (x[i] - us_start_x)/us_slope    # Set elevation for Sloping Segment  U/S Face
-        if us_end_x < x[i] < ds_start_x + 0.1: # FOR The TOP of BANK Segment
-           if top_start_y < y[i] < top_end_y:
-               z[i]=z[i] # Flat Apron
-           else:
-               z[i] +=  z[i]+bank_hgt        # Flat Crest of Embankment
-        if ds_start_x < x[i] < ds_end_x: # DOWN SDLOPE Segment on Downstream face
-            if  top_start_y-(x[i]-ds_start_x)/ds_apron_skew <  y[i]  < top_end_y + (x[i]-ds_start_x)/ds_apron_skew: # Cut Out Segment for Culvert FACE
-               z[i]=z[i] # Flat Apron
+                # Set elevation for Sloping Segment  U/S Face
+                z[i] += z[i] + (x[i] - us_start_x)/us_slope
+        if us_end_x < x[i] < ds_start_x + 0.1:  # FOR The TOP of BANK Segment
+            if top_start_y < y[i] < top_end_y:
+                z[i] = z[i]  # Flat Apron
             else:
-               z[i] += z[i]+bank_hgt-(x[i] -ds_start_x)/ds_slope       # Sloping D/S Face
-           
-        
+                z[i] += z[i]+bank_hgt        # Flat Crest of Embankment
+        if ds_start_x < x[i] < ds_end_x:  # DOWN SDLOPE Segment on Downstream face
+            # Cut Out Segment for Culvert FACE
+            if top_start_y-(x[i]-ds_start_x)/ds_apron_skew < y[i] < top_end_y + (x[i]-ds_start_x)/ds_apron_skew:
+                z[i] = z[i]  # Flat Apron
+            else:
+                z[i] += z[i]+bank_hgt-(x[i] - ds_start_x) / \
+                    ds_slope       # Sloping D/S Face
 
     return z
 
-print ('Setting Quantities....')
+
+print('Setting Quantities....')
 domain.set_quantity('elevation', topography)  # Use function for elevation
-domain.set_quantity('friction', 0.01)         # Constant friction 
+domain.set_quantity('friction', 0.01)         # Constant friction
 domain.set_quantity('stage',
                     expression='elevation')   # Dry initial condition
 
 
-
-
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Setup specialised forcing terms
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Setup CULVERT INLETS and OUTLETS in Current Topography
-#------------------------------------------------------------------------------
-print ('DEFINING any Structures if Required')
+# ------------------------------------------------------------------------------
+print('DEFINING any Structures if Required')
 
 #  DEFINE CULVERT INLET AND OUTLETS
 
@@ -165,15 +164,16 @@ culvert_energy = Culvert_flow(domain,
 domain.forcing_terms.append(culvert_energy)
 """
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Setup boundary conditions
-#------------------------------------------------------------------------------
-print ('Setting Boundary Conditions')
+# ------------------------------------------------------------------------------
+print('Setting Boundary Conditions')
 Br = anuga.Reflective_boundary(domain)              # Solid reflective wall
-Bi = anuga.Dirichlet_boundary([0.0, 0.0, 0.0])          # Inflow based on Flow Depth and Approaching Momentum !!!
+# Inflow based on Flow Depth and Approaching Momentum !!!
+Bi = anuga.Dirichlet_boundary([0.0, 0.0, 0.0])
 
 Bo = anuga.Dirichlet_boundary([-5.0, 0, 0])           # Outflow water at -5.0
-Bd = anuga.Dirichlet_boundary([0,0,0])                # Outflow water at 0.0
+Bd = anuga.Dirichlet_boundary([0, 0, 0])                # Outflow water at 0.0
 
 #Btus = Time_boundary(domain, lambda t: [0.0+ 1.025*(1+num.sin(2*pi*(t-4)/10)), 0.0, 0.0])
 #Btds = Time_boundary(domain, lambda t: [0.0+ 0.0075*(1+num.sin(2*pi*(t-4)/20)), 0.0, 0.0])
@@ -183,9 +183,9 @@ Btds = anuga.Dirichlet_boundary([0.0, 0, 0])           # Outflow water at 1.0
 domain.set_boundary({'left': Btus, 'right': Btds, 'top': Br, 'bottom': Br})
 
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Evolve system through time
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
-for t in domain.evolve(yieldstep = 1, finaltime = 100):
+for t in domain.evolve(yieldstep=1, finaltime=100):
     domain.print_timestepping_statistics()
