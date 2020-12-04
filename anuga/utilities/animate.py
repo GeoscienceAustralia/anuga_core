326409070d13ac3c53047439ce5ac2d7de274fae
Stephen Roberts <stephen.roberts@anu.edu.au>
2020-12-04 20:19:43 +1100

    Update animate.py
    Remove runtime warning

--- anuga/utilities/animate.py
+++ anuga/utilities/animate.py
@@ -42,9 +42,10 @@ class Domain_plotter(object):
         self.ymom = domain.quantities['ymomentum'].centroid_values
         
         self.depth = self.stage - self.elev
-        self.xvel = np.where(self.depth > self.min_depth,
+        with np.errstate(invalid='ignore'):
+            self.xvel = np.where(self.depth > self.min_depth,
                              self.xmom / self.depth, 0.0)
-        self.yvel = np.where(self.depth > self.min_depth,
+            self.yvel = np.where(self.depth > self.min_depth,
                              self.ymom / self.depth, 0.0)
 
         self.speed = np.sqrt(self.xvel**2 + self.yvel**2)        
@@ -426,9 +427,10 @@ class SWW_plotter(object):
             for i in range(self.depth.shape[0]):
                 self.depth[i, :] = self.stage[i, :]-self.elev
 
-        self.xvel = np.where(self.depth > minimum_allowed_depth,
+        with np.errstate(invalid='ignore'):
+            self.xvel = np.where(self.depth > minimum_allowed_depth,
                              self.xmom / self.depth, 0.0)
-        self.yvel = np.where(self.depth > minimum_allowed_depth,
+            self.yvel = np.where(self.depth > minimum_allowed_depth,
                              self.ymom / self.depth, 0.0)
 
         self.speed = np.sqrt(self.xvel**2 + self.yvel**2)
