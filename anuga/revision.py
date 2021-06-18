e0f0ddf87878b8a55b9c962216bd4f93cebe1e85
Stephen Roberts <stoiver@gmail.com>
2021-06-18 15:19:53 +1000

    fixed test_all.py to work with pymetis

--- anuga/revision.py
+++ anuga/revision.py
@@ -4,5 +4,5 @@ This file provides the git sha id and commit date for the installed revision of 
 The file is automatically generated and should not be modified manually.
 """
 
-__git_sha__ = "ab027db2a09a738ea899d038e9a119eab6b8087d"
-__git_committed_datetime__ = "2021-04-13 11:45:36+10:00"
+__git_sha__ = "efe60dcc6a8a5a3bab9bb555c22cc0551417b603"
+__git_committed_datetime__ = "2021-06-18 13:29:59+10:00"
