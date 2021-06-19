4df6fbdf43c64128daf1541499f156a4e31c5c8a
Stephen Roberts <stoiver@gmail.com>
2021-06-19 11:38:12 +1000

    Run tests in install

--- tools/install_ubuntu_20_04.sh
+++ tools/install_ubuntu_20_04.sh
@@ -13,7 +13,7 @@ echo "#==========================="
 echo "# Install packages via apt"
 echo "#==========================="
 
-sudo apt install python-is-python3 python3-pip gfortran netcdf-bin libnetcdf-dev libhdf5-serial-dev gdal-bin libgdal-dev libopenmpi-dev openmpi-bin
+sudo apt install -y -q python-is-python3 python3-pip gfortran netcdf-bin libnetcdf-dev libhdf5-serial-dev gdal-bin libgdal-dev libopenmpi-dev openmpi-bin
 
 #=================================
 # Install python packages via pip3
@@ -23,7 +23,7 @@ echo "#==========================="
 echo "# Install python packages via pip"
 echo "#==========================="
 
-sudo pip3 install scipy matplotlib nose cython netcdf4 matplotlib dill future gitpython pyproj pymetis triangle Pmw mpi4py ipython
+sudo pip3 install -q scipy matplotlib nose cython netcdf4 matplotlib dill future gitpython pyproj pymetis triangle Pmw mpi4py ipython
 
 #=================================
 # Now install anuga. Should be run 
@@ -36,5 +36,11 @@ echo "#==========================="
 
 cd anuga_core
 python setup.py develop --user
+
+echo "#==========================="
+echo "# Run Unit tests"
+echo "#==========================="
+
+python runtests.py
 
 
