c2945fa6cd9a094b0f3eef2635fe6cf6369b0e1f
Stephen Roberts <stoiver@gmail.com>
2021-06-20 21:50:16 +1000

    update revision

--- anuga/revision.py
+++ anuga/revision.py
@@ -4,5 +4,5 @@ This file provides the git sha id and commit date for the installed revision of 
 The file is automatically generated and should not be modified manually.
 """
 
-__git_sha__ = "efe60dcc6a8a5a3bab9bb555c22cc0551417b603"
-__git_committed_datetime__ = "2021-06-18 13:29:59+10:00"
+__git_sha__ = "0e4fcd8c43d8f98a4fab565ebe75dc9ec4dfdb04"
+__git_committed_datetime__ = "2021-06-19 14:37:22+10:00"
