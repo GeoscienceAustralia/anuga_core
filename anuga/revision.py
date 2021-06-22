85b11217655a4863648e9fecdf3a3925e56a5b0a
Stephen Roberts <stoiver@gmail.com>
2021-06-22 20:16:27 +1000

    remove revision

--- anuga/revision.py
+++
@@ -1,8 +0,0 @@
-"""Stored git revision info.
-
-This file provides the git sha id and commit date for the installed revision of ANUGA.
-The file is automatically generated and should not be modified manually.
-"""
-
-__git_sha__ = "7367c0af2d9eb8cf439093db97d628bfb4efa5d8"
-__git_committed_datetime__ = "2021-06-21 20:16:20+10:00"
