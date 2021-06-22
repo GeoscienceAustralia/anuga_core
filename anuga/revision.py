d89e64d5a8bdd3fb46452f5c480bc3708867ddf9
Stephen Roberts <stoiver@gmail.com>
2021-06-22 20:13:25 +1000

    modify gitignore

--- anuga/revision.py
+++ anuga/revision.py
@@ -4,5 +4,5 @@ This file provides the git sha id and commit date for the installed revision of 
 The file is automatically generated and should not be modified manually.
 """
 
-__git_sha__ = "c2945fa6cd9a094b0f3eef2635fe6cf6369b0e1f"
-__git_committed_datetime__ = "2021-06-20 21:50:16+10:00"
+__git_sha__ = "7367c0af2d9eb8cf439093db97d628bfb4efa5d8"
+__git_committed_datetime__ = "2021-06-21 20:16:20+10:00"
