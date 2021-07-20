7ec19b30a203f7e103390beb4c9fc2d5ca0c1fe6
Stephen Roberts <stephen.roberts@anu.edu.au>
2021-07-20 21:32:54 +1000

    Update install_ubuntu_20_04.sh
    Using echo statements as comments

--- tools/install_ubuntu_20_04.sh
+++ tools/install_ubuntu_20_04.sh
@@ -5,37 +5,23 @@
 
 set -e
 
-#================================
-# Install Ubuntu Packages via apt
-#================================
-
 echo "#==========================="
 echo "# Install packages via apt"
 echo "#==========================="
 
 sudo apt install -y -q python-is-python3 python3-pip gfortran netcdf-bin libnetcdf-dev libhdf5-serial-dev gdal-bin libgdal-dev libopenmpi-dev openmpi-bin
 
-#=================================
-# Install python packages via pip3
-#=================================
-
 echo "#==========================="
-echo "# Install python packages via pip"
+echo "# Install python packages via pip3"
 echo "#==========================="
 
 sudo pip3 install scipy matplotlib nose cython netcdf4 matplotlib dill future gitpython pyproj pymetis triangle Pmw mpi4py ipython
 
-#=================================
-# Now install anuga. Should be run 
-# from anuga_core directory
-#=================================
-
 echo "#==========================="
-echo "# Install anuga"
+echo "# Install anuga from the anuga_core directory"
 echo "#==========================="
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 python setup.py develop --user
-
 
 
