08d30c355e1dd287980e307cc9a4fec5342f327d
Stephen Roberts <stephen.roberts@anu.edu.au>
2021-07-27 16:34:19 +1000

    Update install_ubuntu_20_04.sh
    Do anuga install manually

--- tools/install_ubuntu_20_04.sh
+++ tools/install_ubuntu_20_04.sh
@@ -18,10 +18,13 @@ echo "#==========================="
 sudo pip3 install scipy matplotlib nose cython netcdf4 matplotlib dill future gitpython pyproj pymetis triangle Pmw mpi4py ipython
 
 echo "#==========================="
-echo "# Install anuga from the anuga_core directory"
+echo "# You should now install anuga from the anuga_core directory"
+echo "# via"
+echo "cd anuga_core"
+echo "python setup.py install --user"
 echo "#==========================="
 
-cd "$(dirname "${BASH_SOURCE[0]}")"/..
-python setup.py develop --user
+# cd "$(dirname "${BASH_SOURCE[0]}")"/..
+# python setup.py develop --user
 
 
