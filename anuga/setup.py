676c9fcd80f00f65befb4b2b4b676a3d1c7e4181
Stephen Roberts <stoiver@gmail.com>
2022-05-12 21:02:44 +0930

    remove pymetis_4, just use pymetis via pip

--- anuga/setup.py
+++ anuga/setup.py
@@ -26,7 +26,6 @@ def configuration(parent_package='',top_path=None):
     config.add_subpackage('operators')
     config.add_subpackage('parallel')
     config.add_subpackage('pmesh')
-    config.add_subpackage('pymetis_4')
     config.add_subpackage('simulation')
     config.add_subpackage('shallow_water')
     config.add_subpackage('structures')
