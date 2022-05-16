cc2bcc13c4e7f0981073602a9ed7f604c2398895
Stephen Roberts <stoiver@gmail.com>
2022-05-16 21:05:22 +0930

    manually create revision.py

---
+++ anuga/revision.py
@@ -0,0 +1,9 @@
+"""Stored git revision info.
+
+This file provides the git sha id and commit date for the installed revision of ANUGA.
+The file is automatically generated and should not be modified manually.
+"""
+
+__git_sha__ = "79bf1bf2b7acf02fc479b048c3238b25cf5b1ce8"
+__git_committed_datetime__ = "2022-05-16 19:40:46+09:18"
+__version__ = "3.1.2"
