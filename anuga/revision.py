ebabd1aacea850fea05e4ef27980ce1abe06e48f
Stephen Roberts <stoiver@gmail.com>
2022-06-09 18:27:04 +0930

    Updating revision to 3.1.5

--- anuga/revision.py
+++ anuga/revision.py
@@ -4,6 +4,6 @@ This file provides the git sha id and commit date for the installed revision of 
 The file is automatically generated and should not be modified manually.
 """
 
-__git_sha__ = "c2fa9ee5eb0e738708eec24536075675d64c83a7"
-__git_committed_datetime__ = "2022-05-21 17:02:09+09:18"
-__version__ = "3.1.4"
+__git_sha__ = "a9d9181727e6defa7c2f6a38f2f964c49f13698d"
+__git_committed_datetime__ = "2022-06-09 16:51:59+09:18"
+__version__ = "3.1.5"
