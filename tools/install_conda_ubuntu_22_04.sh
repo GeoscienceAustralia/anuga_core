ad7efda9fd41681b997efd26331f71561ca12245
Stephen Roberts <stoiver@gmail.com>
2022-09-10 12:32:18 +0930

    install scripts for Ubuntu 22_04 and fixed up 20_04 scripts

---
+++ tools/install_conda_ubuntu_22_04.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# License: 3-clause BSD
+
+
+set -e
+
+echo "#==========================="
+echo "# Install packages via apt"
+echo "#==========================="
+sudo apt-get update -q
+sudo apt-get install git wget
+
+
+echo "#==========================="
+echo "# Install miniforge"
+echo "#==========================="
+wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3.sh
+
+
+echo "#==========================="
+echo "# Create conda environment anuga_env"
+echo "#==========================="
+conda update --yes conda
+conda create -n anuga_env --yes python=3.10 gxx pip wheel scipy numpy cython netcdf4 pytest nose matplotlib gdal dill future gitpython utm Pmw pymetis meshpy mpi4py
+conda activate anuga_env 
+
+
+echo "#==========================="
+echo "# Installing anuga from the anuga_core directory"
+echo "# and then run unittests"
+echo "#==========================="
+
+cd "$(dirname "${BASH_SOURCE[0]}")"/..
+pip install -e .
+python runtests.py -n
+
+
---
+++ tools/install_ubuntu_22_04.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# License: 3-clause BSD
+
+
+set -e
+
+echo "#==========================="
+echo "# Install packages via apt"
+echo "#==========================="
+
+sudo apt install -y -q build-essential python-dev-is-python3 gfortran netcdf-bin libnetcdf-dev libhdf5-serial-dev gdal-bin libgdal-dev libopenmpi-dev openmpi-bin
+
+echo "#==========================="
+echo "# Create a virtual environment and then"
+echo "# install python packages via pip"
+echo "#==========================="
+
+cd "$(dirname "${BASH_SOURCE[0]}")"/..
+python3 -m venv anuga_env
+source anuga_env/bin/activate
+pip install wheel scipy gdal==3.4.1 matplotlib pytest cython netcdf4 matplotlib dill future gitpython pyproj pymetis pybind11 meshpy Pmw  ipython utm mpi4py
+
+echo "#==========================="
+echo "# Installing anuga from the anuga_core directory"
+echo "# and then run unittests"
+echo "#==========================="
+
+pip install -e .
+python runtests.py -n
+
+echo "#================================================"
+echo "# To use anuga you must activate the"
+echo "# python environment anuga_env that has been"
+echo "# created in your anuga_core directory by"
+echo "# changing to anuga_core directory then running:"
+echo "# source anuga_env/bin/activate"
+echo "#================================================"
+
+
