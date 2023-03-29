c515cc3d53510b2384f27905125aadbb5cedb285
Stephen Roberts <stoiver@gmail.com>
2023-03-29 17:52:43 +1030

    fix test_distribute_mesh with current meshpy

--- anuga/parallel/tests/test_distribute_mesh.py
+++ anuga/parallel/tests/test_distribute_mesh.py
@@ -202,15 +202,33 @@ class Test_Distribute_Mesh(unittest.TestCase):
                                             [8, 12,  7],
                                             [5, 12,  8]])
 
+        true_triangles_meshpy_2022_1_3 = array([[ 0,  9,  1],
+       [ 3,  9,  0],
+       [ 4,  9,  3],
+       [ 1,  9,  4],
+       [ 4, 10,  1],
+       [ 5, 10,  4],
+       [ 3, 11,  4],
+       [ 4, 12,  5],
+       [ 7, 12,  4],
+       [ 1, 10,  2],
+       [ 2, 10,  5],
+       [ 6, 11,  3],
+       [ 7, 11,  6],
+       [ 4, 11,  7],
+       [ 8, 12,  7],
+       [ 5, 12,  8]])
+
         assert num.allclose(nodes,true_nodes)
 
-        #pprint(triangles)
+        pprint(triangles)
         assert num.allclose(triangles, true_triangles_4) or \
             num.allclose(triangles, true_triangles_5) or \
             num.allclose(triangles, true_triangles_win_4) or \
-            num.allclose(triangles, true_triangles_5_part_mesh)
+            num.allclose(triangles, true_triangles_5_part_mesh) or \
+            num.allclose(triangles, true_triangles_meshpy_2022_1_3)
 
-        #print(triangles_per_proc)
+        print(triangles_per_proc)
         assert num.allclose(triangles_per_proc,[8,8]) or num.allclose(triangles_per_proc,[9,7])
 
 
@@ -628,7 +646,7 @@ class Test_Distribute_Mesh(unittest.TestCase):
         boundary = {(13, 1): 'bottom', (7, 1): 'left', (3, 1): 'right', (14, 1): 'right', \
         (11, 1): 'bottom', (10, 1): 'top', (5, 1): 'left', (4, 1): 'top'}
 
-        """
+        r"""
                      top
             (10,1)       (4,1)
         2 ---------- 5 ---------- 8
