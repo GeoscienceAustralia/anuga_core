8cb4718a88dcc19b262d49b4d9487d952c90ac88
Stephen Roberts <stoiver@gmail.com>
2023-04-26 17:06:13 +0930

    added meshpy 2022_1_3_win distribution for test_distribute_mesh

--- anuga/parallel/tests/test_distribute_mesh.py
+++ anuga/parallel/tests/test_distribute_mesh.py
@@ -202,31 +202,50 @@ class Test_Distribute_Mesh(unittest.TestCase):
                                             [8, 12,  7],
                                             [5, 12,  8]])
 
-        true_triangles_meshpy_2022_1_3 = array([[ 0,  9,  1],
-       [ 3,  9,  0],
-       [ 4,  9,  3],
-       [ 1,  9,  4],
-       [ 4, 10,  1],
-       [ 5, 10,  4],
-       [ 3, 11,  4],
-       [ 4, 12,  5],
-       [ 7, 12,  4],
-       [ 1, 10,  2],
-       [ 2, 10,  5],
-       [ 6, 11,  3],
-       [ 7, 11,  6],
-       [ 4, 11,  7],
-       [ 8, 12,  7],
-       [ 5, 12,  8]])
+        true_triangles_meshpy_2022_1_3_win = array([[0,  9,  1],
+                                                    [3,  9,  0],
+                                                    [4,  9,  3],
+                                                    [1,  9,  4],
+                                                    [1, 10,  2],
+                                                    [4, 10,  1],
+                                                    [5, 10,  4],
+                                                    [2, 10,  5],
+                                                    [3, 11,  4],
+                                                    [6, 11,  3],
+                                                    [7, 11,  6],
+                                                    [4, 11,  7],
+                                                    [4, 12,  5],
+                                                    [7, 12,  4],
+                                                    [8, 12,  7],
+                                                    [5, 12,  8]])
+
+        true_triangles_meshpy_2022_1_3 = array([[0,  9,  1],
+                                                [3,  9,  0],
+                                                [4,  9,  3],
+                                                [1,  9,  4],
+                                                [4, 10,  1],
+                                                [5, 10,  4],
+                                                [3, 11,  4],
+                                                [4, 12,  5],
+                                                [7, 12,  4],
+                                                [1, 10,  2],
+                                                [2, 10,  5],
+                                                [6, 11,  3],
+                                                [7, 11,  6],
+                                                [4, 11,  7],
+                                                [8, 12,  7],
+                                                [5, 12,  8]])
 
         assert num.allclose(nodes,true_nodes)
 
         pprint(triangles)
+
         assert num.allclose(triangles, true_triangles_4) or \
             num.allclose(triangles, true_triangles_5) or \
             num.allclose(triangles, true_triangles_win_4) or \
             num.allclose(triangles, true_triangles_5_part_mesh) or \
-            num.allclose(triangles, true_triangles_meshpy_2022_1_3)
+            num.allclose(triangles, true_triangles_meshpy_2022_1_3) or \
+            num.allclose(triangles, true_triangles_meshpy_2022_1_3_win)
 
         print(triangles_per_proc)
         assert num.allclose(triangles_per_proc,[8,8]) or num.allclose(triangles_per_proc,[9,7])
