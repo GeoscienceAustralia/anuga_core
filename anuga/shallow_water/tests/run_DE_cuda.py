9ba66619994fdd2c2007cbbbaaacfbecee900f75
Stephen Roberts <stephen.roberts@anu.edu.au>
2023-06-13 19:36:45 +0930

    local_timestep -> timestep_array

--- anuga/shallow_water/tests/run_DE_cuda.py
+++ anuga/shallow_water/tests/run_DE_cuda.py
@@ -387,7 +387,7 @@ def compute_fluxes_ext_central_kernel(domain, timestep):
     nvtxRangePush('calculate flux: reduction operations')
     
     if substep_count == 0:
-        timestep = local_timestep.min()
+        timestep = timestep_array.min()
 
     domain.boundary_flux_sum[substep_count] = local_boundary_flux_sum.sum()
 
