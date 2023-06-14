cd619a9ef6031cc3498e5b14abbeecdf0dfdede0
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2023-06-14 13:45:24 +1000

    Removed all remaining references to shallow_water_ext from shallow water tests

--- anuga/shallow_water/tests/test_shallow_water_domain.py
+++ anuga/shallow_water/tests/test_shallow_water_domain.py
@@ -3964,7 +3964,7 @@ class Test_Shallow_Water(unittest.TestCase):
         # Import underlying routine locally.
         # FIXME (Ole): This routine might have to be removed entirely as likely
         # superseded by extrapolate_second_order_edge_sw
-        from  anuga.shallow_water.swDE1_domain_ext import extrapolate_second_order_sw
+        from anuga.shallow_water.swDE1_domain_ext import extrapolate_second_order_sw
 
         extrapolate_second_order_sw(domain)
 
