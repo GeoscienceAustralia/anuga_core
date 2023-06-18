ba23391263eec7c0789a0e513e9c700700953601
Stephen Roberts <stephen.roberts@anu.edu.au>
2023-06-18 13:01:03 +0930

    clean stage and xmom prints

--- anuga/shallow_water/tests/run_DE_cuda.py
+++ anuga/shallow_water/tests/run_DE_cuda.py
@@ -428,13 +428,15 @@ print('stage explicit update L2error ', num.linalg.norm(stage1.explicit_update-s
 print('xmom  explicit update L2error ', num.linalg.norm(xmom1.explicit_update-xmom2.explicit_update)*sqrtN)
 print('ymom  explicit update L2error ', num.linalg.norm(ymom1.explicit_update-ymom2.explicit_update)*sqrtN)
 
-print('stage explicit update inferror ', num.linalg.norm(stage1.explicit_update-stage2.explicit_update,num.inf))
-print('xmom  explicit update inferror ', num.linalg.norm(xmom1.explicit_update-xmom2.explicit_update,num.inf))
-print('ymom  explicit update inferror ', num.linalg.norm(ymom1.explicit_update-ymom2.explicit_update,num.inf))
+print('stage update inferror         ', num.linalg.norm(stage1.explicit_update-stage2.explicit_update,num.inf))
+print('xmom  update inferror         ', num.linalg.norm(xmom1.explicit_update-xmom2.explicit_update,num.inf))
+print('ymom  update inferror         ', num.linalg.norm(ymom1.explicit_update-ymom2.explicit_update,num.inf))
 #print('edge timestep error         ', num.linalg.norm(domain1.edge_timestep-domain2.edge_timestep))
 #print('pressure work error         ', num.linalg.norm(domain1.pressuregrad_work-domain2.pressuregrad_work))
 #print('edge flux work error        ', num.linalg.norm(domain1.edge_flux_work-domain2.edge_flux_work))
 
+
+#(num.abs(stage1.explicit_update-stage2.explicit_update)/num.abs(stage1.explicit_update)).max()
 
 from pprint import pprint
 
@@ -447,21 +449,22 @@ if True:
     pprint((max_speed_1-max_speed_2).reshape(2*nx,2*ny))
 
 
-
 stage_ids = num.argsort(num.abs(stage1.explicit_update-stage2.explicit_update))
 
+print('stage max diff values')
 pprint(stage_ids[-10:])
-pprint(stage1.explicit_update[-10:])
-pprint(stage2.explicit_update[-10:])
+pprint(stage1.explicit_update[stage_ids[-10:]])
+pprint(stage2.explicit_update[stage_ids[-10:]])
 pprint(num.abs(stage1.explicit_update-stage2.explicit_update)[stage_ids[-10:]])
 print(num.abs(stage1.explicit_update-stage2.explicit_update).max())
 
 
 xmom_ids = num.argsort(num.abs(xmom1.explicit_update-xmom2.explicit_update))
 
+print('xmom max diff values')
 pprint(xmom_ids[-10:])
-pprint(xmom1.explicit_update[-10:])
-pprint(xmom2.explicit_update[-10:])
+pprint(xmom1.explicit_update[xmom_ids[-10:]])
+pprint(xmom2.explicit_update[xmom_ids[-10:]])
 pprint(num.abs(xmom1.explicit_update-xmom2.explicit_update)[xmom_ids[-10:]])
 print(num.abs(xmom1.explicit_update-xmom2.explicit_update).max())
 
