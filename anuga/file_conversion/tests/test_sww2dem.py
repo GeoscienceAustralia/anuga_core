02ab135dc200aae75ed37f6fef7d5c56342d39b9
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2023-06-19 10:29:20 +1000

    One more obsolete test deprecated

--- anuga/file_conversion/tests/test_sww2dem.py
+++ anuga/file_conversion/tests/test_sww2dem.py
@@ -2186,7 +2186,6 @@ Statistics of SWW file:
 if __name__ == "__main__":
     # suite = unittest.makeSuite(Test_Shallow_Water, 'test_rainfall_forcing_with_evolve')
 
-
-    suite = unittest.makeSuite(Test_Sww2Dem, 'test_sww2dem_asc_elevation_depth')
+    suite = unittest.makeSuite(Test_Sww2Dem, 'test')
     runner = unittest.TextTestRunner(verbosity=1)
     runner.run(suite)
