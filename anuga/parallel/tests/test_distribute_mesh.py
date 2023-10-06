83d8ba09716d4ac1b74191b7d2160de8af9c0028
Stephen Roberts <stephen.roberts@anu.edu.au>
2023-10-06 16:44:34 +1030

    update triangle array with new version of meshpy

--- anuga/parallel/tests/test_distribute_mesh.py
+++ anuga/parallel/tests/test_distribute_mesh.py
@@ -254,6 +254,24 @@ class Test_Distribute_Mesh(unittest.TestCase):
                                                 [8, 12,  7],
                                                 [5, 12,  8]])
 
+        true_triangles_meshpy_2022_1_4 = array([[ 0,  9,  1],
+                                                [ 3,  9,  0],
+                                                [ 1,  9,  4],
+                                                [ 1, 10,  2],
+                                                [ 4, 10,  1],
+                                                [ 5, 10,  4],
+                                                [ 2, 10,  5],
+                                                [ 4,  9,  3],
+                                                [ 3, 11,  4],
+                                                [ 6, 11,  3],
+                                                [ 7, 11,  6],
+                                                [ 4, 11,  7],
+                                                [ 4, 12,  5],
+                                                [ 7, 12,  4],
+                                                [ 8, 12,  7],
+                                                [ 5, 12,  8]])
+
+
         assert num.allclose(nodes,true_nodes)
 
         pprint(triangles)
@@ -263,6 +281,7 @@ class Test_Distribute_Mesh(unittest.TestCase):
             num.allclose(triangles, true_triangles_win_4) or \
             num.allclose(triangles, true_triangles_5_part_mesh) or \
             num.allclose(triangles, true_triangles_meshpy_2022_1_3) or \
+            num.allclose(triangles, true_triangles_meshpy_2022_1_4) or \
             num.allclose(triangles, true_triangles_meshpy_2022_1_3_win_1) or \
             num.allclose(triangles, true_triangles_meshpy_2022_1_3_win_2)
             
