0381c58d004b6bf647fb9bb5ea180b7bd895eddf
Stephen Roberts <stephen.roberts@anu.edu.au>
2023-10-21 19:20:34 +1030

    switch extrapolation calc to simd

--- anuga/shallow_water/shallow_water_domain.py
+++ anuga/shallow_water/shallow_water_domain.py
@@ -1794,8 +1794,8 @@ class Domain(Generic_Domain):
             from .sw_domain_openacc_ext import compute_fluxes_ext_central
         elif self.multiprocessor_mode == 4:
             # change over to cuda routines as developed
-            from .sw_domain_simd_ext import compute_fluxes_ext_central
-            #compute_fluxes_ext_central = self.gpu_interface.compute_fluxes_ext_central_kernel
+            #from .sw_domain_simd_ext import compute_fluxes_ext_central
+            compute_fluxes_ext_central = self.gpu_interface.compute_fluxes_ext_central_kernel
         else:
             raise Exception('Not implemented')
 
@@ -1833,8 +1833,8 @@ class Domain(Generic_Domain):
 
         elif self.multiprocessor_mode == 4:
             # change over to cuda routines as developed
-            #from .sw_domain_simd_ext import extrapolate_second_order_edge_sw
-            extrapolate_second_order_edge_sw = self.gpu_interface.extrapolate_second_order_edge_sw_kernel
+            from .sw_domain_simd_ext import extrapolate_second_order_edge_sw
+            #extrapolate_second_order_edge_sw = self.gpu_interface.extrapolate_second_order_edge_sw_kernel
             extrapolate_second_order_edge_sw(self)
         else:
             raise Exception('Not implemented')
