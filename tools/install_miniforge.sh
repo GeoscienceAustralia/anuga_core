120c8b1ac5fc801b6b42623858925723324104db
Stephen Roberts <stephen.roberts@anu.edu.au>
2025-02-16 13:07:44 +1100

    Test for previous miniforge3 install

--- tools/install_miniforge.sh
+++ tools/install_miniforge.sh
@@ -7,53 +7,64 @@ set -e
 
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
+ANUGA_CORE_PATH=$(realpath "$SCRIPTPATH/..")
 
 
 echo "#==========================="
-echo "# Install miniforge"
+echo "# Install miniforge3"
 echo "#==========================="
 cd $HOME
-#wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-#bash Miniforge3.sh
+
+if [ -f "$HOME/Miniforge3.sh" ]; then
+     echo "Miniforge3.sh already exists."
+else
+     echo "Miniforge3.sh does not exist. Downloading..."
+     wget -O "$HOME/Miniforge3.sh" "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+fi
 
 
-echo "#=========================================="
-echo "# create conda/mamba environment anuga_env"
-echo "#=========================================="
+if [ -d "$HOME/miniforge3" ]; then
+     echo "miniforge3 already exists."
+else
+     echo "Miniforge does not exist. Installing from Miniforge3.sh..."
+     bash Miniforge3.sh
+fi
+
+
+echo "#==============================================="
+echo "# create conda/mamba environment anuga_env_${PY}"
+echo "#==============================================="
+echo "..."
 mamba create -n anuga_env_${PY} --yes python=${PY} compilers pip numpy scipy cython netcdf4 \
      nose matplotlib gdal dill gitpython mpi4py utm Pmw pymetis meshpy pytest pyproj affine \
      meson-python meson ninja xarray future
 
-echo "#================================"
+echo "#======================================"
 echo "# activate environment anuga_env_${PY}"
-echo "#================================"
-
+echo "#======================================"
+echo "..."
 source ./miniforge3/bin/activate anuga_env_${PY}
 
 
-# echo "#================================================"
-# echo `pwd`
-# echo "#================================================"
-
-# Useful for debugging any issues with conda
-#mamba info -a
-
-echo "#================================================"
-echo "# Installing anuga from the anuga_core directory"
-echo "#================================================"
+echo "#================================================================"
+echo "# Installing anuga from the ${ANUGA_CORE_PATH} directory"
+echo "#================================================================"
+echo "..."
 
 cd ${SCRIPTPATH}
 cd ..
-echo "#================================================"
 pip install .
-echo "#================================================"
+echo " "
 
 echo "#==========================="
 echo "# Run unittests"
 echo "#==========================="
+echo " "
+
 cd ..
 pytest -q --disable-warnings --pyargs anuga
 
+echo " "
 echo "#=================================================================="
 echo "# Congratulations, Looks like you have successfully installed anuga"
 echo "#=================================================================="
@@ -77,5 +88,5 @@ echo "# the mamba command"
 echo "# "
 echo "# mamba activate anuga_env_${PY}"
 echo "# "
-echo "# to activate the anuga_env environment"
+echo "# to activate the anuga_env_${PY} environment"
 echo "#=================================================================="
