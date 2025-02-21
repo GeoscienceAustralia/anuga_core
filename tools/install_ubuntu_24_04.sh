34b9d8ad4e940dc35944e296e31faf15f965190c
Stephen Roberts <stephen.roberts@anu.edu.au>
2025-02-21 20:22:47 +1100

    try numpy==1.26.4 on 24_04

--- tools/install_ubuntu_24_04.sh
+++ tools/install_ubuntu_24_04.sh
@@ -9,12 +9,34 @@ echo "#==========================="
 echo "# Install packages via apt"
 echo "#==========================="
 
-sudo apt install -y -q build-essential python-dev-is-python3 gfortran netcdf-bin libnetcdf-dev libhdf5-serial-dev gdal-bin libgdal-dev libopenmpi-dev openmpi-bin python3-venv
+# sudo apt install -y -q build-essential python-dev-is-python3 gfortran netcdf-bin \
+#     libnetcdf-dev libhdf5-serial-dev gdal-bin libgdal-dev libopenmpi-dev openmpi-bin \
+#     python3-venv
 
-echo "#==========================="
+sudo apt-get install -y build-essential cmake python3-dev python-dev-is-python3 \
+     netcdf-bin libnetcdf-dev libhdf5-serial-dev libpq-dev libgeos-dev libexpat-dev \
+    libxerces-c-dev libwebp-dev libpng-dev libzstd-dev libssl-dev libopenjp2-7-dev \
+    libspatialite-dev libmuparser-dev autoconf automake sqlite3 bash-completion swig \
+    libopenmpi-dev openmpi-bin python3-venv
+
+#build gdal from source
+# cd /tmp
+# git clone https://github.com/OSGeo/gdal.git
+# cd gdal
+# mkdir build
+# cd build
+
+# cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
+# cmake --build .
+# sudo cmake --install .
+# sudo ldconfig
+# gdalinfo --version
+
+
+echo "#======================================="
 echo "# Create a virtual environment and then"
 echo "# install python packages via pip"
-echo "#==========================="
+echo "#======================================="
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
@@ -27,7 +49,7 @@ PKG_CONFIG_PATH="${ANUGA_CORE_PATH}/anuga_env/lib/python3.12/site-packages/numpy
 
 python3 -m venv anuga_env
 source anuga_env/bin/activate
-pip install wheel numpy==2.2 scipy gdal==3.8 matplotlib pytest cython netcdf4 \
+pip install wheel numpy==1.26.4 --force-reinstall scipy gdal==3.8 matplotlib pytest cython netcdf4 \
      matplotlib dill future gitpython pyproj pymetis pybind11 meshpy Pmw ipython \
      utm affine mpi4py xarray meson meson-python ninja
 
