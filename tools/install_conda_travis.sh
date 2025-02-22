234d8790c4139373721ed574460605e573c0b7f5
Stephen Roberts <stephen.roberts@anu.edu.au>
2025-02-22 12:16:24 +1100

    update travis tests

--- tools/install_conda_travis.sh
+++ tools/install_conda_travis.sh
@@ -20,6 +20,8 @@ deactivate || echo "deactivate failed"
 
 # Use the miniforge installer
 wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+
+
 chmod +x Miniforge3.sh && ./Miniforge3.sh -b
 
 export PATH=/home/travis/miniforge3/bin:$PATH 
@@ -31,11 +33,15 @@ source ~/.bashrc
 conda update --yes conda
 
 # Configure the conda environment
-conda create -n anuga_env --yes python=$PYTHON_VERSION pip numpy scipy meshpy cython netcdf4 pytest matplotlib gdal dill gitpython Pmw pymetis utm pyproj affine xarray mpi4py
+conda create -n anuga_env --yes python=$PYTHON_VERSION compilers numpy scipy cython netcdf4 \
+     nose matplotlib gdal dill gitpython mpi4py utm Pmw pymetis meshpy pytest pyproj affine \
+     meson-python meson ninja xarray future pkg-config
+     
+#conda create -n anuga_env --yes python=$PYTHON_VERSION pip numpy scipy meshpy cython netcdf4 pytest matplotlib gdal dill gitpython Pmw pymetis utm pyproj affine xarray mpi4py
 conda activate anuga_env
 
 # Useful for debugging any issues with conda
 conda info -a
 
 # Build and install anuga
-pip install -e .
+pip install .
